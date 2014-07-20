185f22bc3cde11a823dc6a89eff1c29588607288
Adrian Danis <Adrian.Danis@nicta.com.au>
2014-07-21 09:38:36 +1000

    Add license header

--- src/arch/arm/machine/errata.c
+++ src/arch/arm/machine/errata.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2014, General Dynamics C4 Systems
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(GD_GPL)
+ */
 
 #include <config.h>
 #include <api/types.h>
