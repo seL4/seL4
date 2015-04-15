efcacb55ba71ffe932675739308f2501d8fc34ee
Sean Peters <sean.peters@nicta.com.au>
2015-04-15 11:56:31 +1000

    split the types header into basic and compound to resolve circular dependencies

---
+++ include/basic_types.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2014, General Dynamics C4 Systems
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(GD_GPL)
+ */
+
+#ifndef __BASIC_TYPES_H
+#define __BASIC_TYPES_H
+
+#include <stdint.h>
+#include <arch/types.h>
+
+enum _bool {
+    false = 0,
+    true  = 1
+};
+typedef uint32_t bool_t;
+
+typedef struct region {
+    pptr_t start;
+    pptr_t end;
+} region_t;
+
+typedef struct p_region {
+    paddr_t start;
+    paddr_t end;
+} p_region_t;
+
+typedef struct v_region {
+    vptr_t start;
+    vptr_t end;
+} v_region_t;
+
+#define REG_EMPTY (region_t){ .start = 0, .end = 0 }
+#define P_REG_EMPTY (p_region_t){ .start = 0, .end = 0 }
+
+#endif /* __BASIC_TYPES_H */
