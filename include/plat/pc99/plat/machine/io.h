fc1e413e5b3a23465cf8eca90d3a053650a1c75f
Adrian Danis <Adrian.Danis@nicta.com.au>
2015-05-12 17:10:19 +1000

    ia32: Move many assembly operations from .S file into inline assembly

--- include/plat/pc99/plat/machine/io.h
+++ include/plat/pc99/plat/machine/io.h
@@ -21,8 +21,19 @@ uint16_t in16(uint16_t port);
 uint32_t in32(uint16_t port);
 
 /* these versions are linked to physical addresses */
-void out8_phys(uint16_t port, uint8_t value);
-uint8_t in8_phys(uint16_t port);
+static inline void SECTION(".phys.text")
+out8_phys(uint16_t port, uint8_t value)
+{
+    asm volatile("outb %[value], %[port]" :: [port] "d"(port), [value] "a"(value));
+}
+
+static inline uint8_t SECTION(".phys.text")
+in8_phys(uint16_t port)
+{
+    uint8_t value;
+    asm volatile("inb %[port], %[value]" : [value] "=a"(value) : [port] "d" (port));
+    return value;
+}
 
 #ifdef DEBUG
 
