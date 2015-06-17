ff0896c6ae3bf29f63d9bca666c9399f365c1619
Adrian Danis <Adrian.Danis@nicta.com.au>
2015-06-17 16:31:05 +1000

    x86: Create 32-bit specific statedata

---
+++ src/arch/x86/model/statedata_32.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2014, General Dynamics C4 Systems
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(GD_GPL)
+ */
+
+#include <arch/model/statedata.h>
+
+/* The privileged kernel mapping PD & PT */
+pdpte_t ia32KSGlobalPDPT[BIT(PDPT_BITS)] ALIGN(BIT(PDPT_SIZE_BITS));
+pde_t ia32KSGlobalPD[BIT(PD_BITS + PDPT_BITS)] ALIGN(BIT(PD_SIZE_BITS));
+pte_t ia32KSGlobalPT[BIT(PT_BITS)] ALIGN(BIT(PT_SIZE_BITS));
