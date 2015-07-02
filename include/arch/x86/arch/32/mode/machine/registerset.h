022287c38b23d173767f366cdc7dfba881abbbd7
Adrian Danis <Adrian.Danis@nicta.com.au>
2015-07-02 15:09:01 +1000

    x86: Minimal changes to fix previous commit that renamed headers

--- include/arch/x86/arch/32/mode/machine/registerset.h
+++ include/arch/x86/arch/32/mode/machine/registerset.h
@@ -8,8 +8,8 @@
  * @TAG(GD_GPL)
  */
 
-#ifndef __ARCH_MACHINE_REGISTERSET_H
-#define __ARCH_MACHINE_REGISTERSET_H
+#ifndef __MODE_MACHINE_REGISTERSET_H
+#define __MODE_MACHINE_REGISTERSET_H
 
 #include <arch/types.h>
 #include <util.h>
