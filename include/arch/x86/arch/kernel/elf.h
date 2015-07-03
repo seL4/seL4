17db2e5a342722ff22a02f07d140bdf63af0fc37
Adrian Danis <Adrian.Danis@nicta.com.au>
2015-07-03 11:42:07 +1000

    x64: 64-bit elf loading

--- include/arch/x86/arch/kernel/elf.h
+++ include/arch/x86/arch/kernel/elf.h
@@ -43,10 +43,42 @@ typedef struct Elf32_Phdr {
     uint32_t      p_align;     /* Reqd alignment of segment in memory */
 } Elf32_Phdr_t;
 
+typedef struct Elf64_Header {
+    unsigned char       e_ident[16];
+    uint16_t            e_type;
+    uint16_t            e_machine;
+    uint32_t            e_version;
+    uint64_t            e_entry;
+    uint64_t            e_phoff;
+    uint64_t            e_shoff;
+    uint32_t            e_flags;
+    uint16_t            e_ehsize;
+    uint16_t            e_phentsize;
+    uint16_t            e_phnum;
+    uint16_t            e_shentsize;
+    uint16_t            e_shnum;
+    uint16_t            e_shstrndx;
+} Elf64_Header_t;
+
+typedef struct Elf64_Phdr {
+    uint32_t            p_type;
+    uint32_t            p_flags;
+    uint64_t            p_offset;
+    uint64_t            p_vaddr;
+    uint64_t            p_paddr;
+    uint64_t            p_filesz;
+    uint64_t            p_memsz;
+    uint64_t            p_align;
+} Elf64_Phdr_t;
+
 /* minimal ELF functionality for loading GRUB boot module */
 
 bool_t elf32_checkFile(Elf32_Header_t* elfFile);
 v_region_t elf32_getMemoryBounds(Elf32_Header_t* elfFile);
 void elf32_load(Elf32_Header_t* elfFile, int32_t offset);
+
+bool_t elf64_checkFile(Elf64_Header_t *elf);
+v_region_t  elf64_getMemoryBounds(Elf64_Header_t *elf);
+void elf64_load(Elf64_Header_t *elf, uint64_t offset);
 
 #endif
