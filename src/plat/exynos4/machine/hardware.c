311c9d7b11ed16ebf34ab977c24ae37e3033894e
akroh <Alexander.Kroh@nicta.com.au>
2015-07-27 18:35:39 +1000

    EXYNOS4: Enlarge PMU device region

--- src/plat/exynos4/machine/hardware.c
+++ src/plat/exynos4/machine/hardware.c
@@ -40,7 +40,7 @@ get_avail_p_reg(unsigned int i)
 const p_region_t BOOT_RODATA dev_p_regs[] = {
     { /* .start */ CHIPID_PADDR           , /* .end */ CHIPID_PADDR            + (1 << PAGE_BITS) },
     { /* .start */ SYSREG_PADDR           , /* .end */ SYSREG_PADDR            + (1 << PAGE_BITS) },
-    { /* .start */ PMU_PADDR              , /* .end */ PMU_PADDR               + (1 << PAGE_BITS) },
+    { /* .start */ PMU_PADDR              , /* .end */ PMU_PADDR               + (5 << PAGE_BITS) },
     { /* .start */ CMU_TOP_PART_PADDR     , /* .end */ CMU_TOP_PART_PADDR      + (13 << PAGE_BITS) },
     { /* .start */ SMDMA0_PADDR           , /* .end */ SMDMA0_PADDR            + (1 << PAGE_BITS) },
     { /* .start */ CMU_DMC_PART_PADDR     , /* .end */ CMU_DMC_PART_PADDR      + (9 << PAGE_BITS) },
