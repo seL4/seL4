c58098c97a904dceac8d99fc8b4869764a7d1279
Adrian Danis <Adrian.Danis@nicta.com.au>
2015-11-04 15:16:38 +1100

    x86: Factor out IA32 specific objecttype code

--- src/arch/x86/object/objecttype.c
+++ src/arch/x86/object/objecttype.c
@@ -64,12 +64,6 @@ deriveCap_ret_t Arch_deriveCap(cte_t* slot, cap_t cap)
         }
         return ret;
 
-    case cap_frame_cap:
-        cap = cap_frame_cap_set_capFIsIOSpace(cap, 0);
-        ret.cap = cap_frame_cap_set_capFMappedASID(cap, asidInvalid);
-        ret.status = EXCEPTION_NONE;
-        return ret;
-
     case cap_asid_control_cap:
     case cap_asid_pool_cap:
         ret.cap = cap;
@@ -95,9 +89,7 @@ deriveCap_ret_t Arch_deriveCap(cte_t* slot, cap_t cap)
         return ret;
 
     default:
-        /* This assert has no equivalent in haskell,
-         * as the options are restricted by type */
-        fail("Invalid arch cap type");
+        return Mode_deriveCap(slot, cap);
     }
 }
 
@@ -159,16 +151,8 @@ cap_t CONST Arch_maskCapRights(cap_rights_t cap_rights_mask, cap_t cap)
 
 cap_t Arch_finaliseCap(cap_t cap, bool_t final)
 {
-    switch (cap_get_capType(cap)) {
-    case cap_pdpt_cap:
-        if (final && cap_pdpt_cap_get_capPDPTIsMapped(cap)) {
-            deleteASID(
-                cap_pdpt_cap_get_capPDPTMappedASID(cap),
-                (vspace_root_t*)cap_pdpt_cap_get_capPDPTBasePtr(cap)
-            );
-        }
-        break;
 
+    switch (cap_get_capType(cap)) {
     case cap_page_directory_cap:
         if (final && cap_page_directory_cap_get_capPDIsMapped(cap)) {
             unmapPageDirectory(
@@ -189,21 +173,6 @@ cap_t Arch_finaliseCap(cap_t cap, bool_t final)
         }
         break;
 
-    case cap_frame_cap:
-        if (cap_frame_cap_get_capFMappedASID(cap)) {
-            if (cap_frame_cap_get_capFIsIOSpace(cap)) {
-                unmapIOPage(cap);
-                break;
-            }
-            unmapPage(
-                cap_frame_cap_get_capFSize(cap),
-                cap_frame_cap_get_capFMappedASID(cap),
-                cap_frame_cap_get_capFMappedAddress(cap),
-                (void *)cap_frame_cap_get_capFBasePtr(cap)
-            );
-        }
-        break;
-
     case cap_asid_pool_cap:
         if (final) {
             deleteASIDPool(
@@ -217,6 +186,7 @@ cap_t Arch_finaliseCap(cap_t cap, bool_t final)
         break;
     case cap_io_space_cap:
         break;
+
     case cap_io_page_table_cap:
         if (final && cap_io_page_table_cap_get_capIOPTIsMapped(cap)) {
             deleteIOPageTable(cap);
@@ -224,13 +194,13 @@ cap_t Arch_finaliseCap(cap_t cap, bool_t final)
         break;
 
     default:
-        fail("Invalid arch cap type");
+        return Mode_finaliseCap(cap, final);
     }
 
     return cap_null_cap_new();
 }
 
-static cap_t CONST
+cap_t CONST
 resetMemMapping(cap_t cap)
 {
     switch (cap_get_capType(cap)) {
@@ -247,7 +217,7 @@ resetMemMapping(cap_t cap)
         return cap_pdpt_cap_set_capPDPTIsMapped(cap, 0);
     }
 
-    return cap;
+    return Mode_resetMemMapping(cap);
 }
 
 cap_t Arch_recycleCap(bool_t is_final, cap_t cap)
@@ -291,14 +261,6 @@ cap_t Arch_recycleCap(bool_t is_final, cap_t cap)
         }
         return cap;
 
-    case cap_pdpt_cap:
-        clearMemory((void*)cap_get_capPtr(cap), cap_get_capSizeBits(cap));
-        Arch_finaliseCap(cap, is_final);
-        if (is_final) {
-            return resetMemMapping(cap);
-        }
-        return cap;
-
     case cap_asid_control_cap:
         return cap;
 
@@ -325,7 +287,7 @@ cap_t Arch_recycleCap(bool_t is_final, cap_t cap)
         return cap;
 
     default:
-        fail("Invalid arch cap type");
+        return Mode_recycleCap(is_final, cap);
     }
 }
 
@@ -413,7 +375,7 @@ bool_t CONST Arch_sameRegionAs(cap_t cap_a, cap_t cap_b)
 
     }
 
-    return false;
+    return Mode_sameRegionAs(cap_a, cap_b);
 }
 
 bool_t CONST Arch_sameObjectAs(cap_t cap_a, cap_t cap_b)
@@ -438,98 +400,22 @@ Arch_getObjectSize(word_t t)
     case seL4_IA32_LargePage:
         return pageBitsForSize(IA32_LargePage);
     case seL4_IA32_PageTableObject:
-        return PTE_SIZE_BITS + PT_BITS;
+        return PT_SIZE_BITS;
     case seL4_IA32_PageDirectoryObject:
-        return PDE_SIZE_BITS + PD_BITS;
+        return PD_SIZE_BITS;
     case seL4_IA32_PDPTObject:
-        return PDPTE_SIZE_BITS + PDPT_BITS;
+        return PDPT_SIZE_BITS;
     case seL4_IA32_IOPageTableObject:
         return VTD_PT_SIZE_BITS;
     default:
-        fail("Invalid object type");
-        return 0;
+        return Mode_getObjectSize(t);
     }
 }
 
 cap_t
 Arch_createObject(object_t t, void *regionBase, word_t userSize)
 {
-    switch (t) {
-    case seL4_IA32_4K:
-        memzero(regionBase, 1 << pageBitsForSize(IA32_SmallPage));
-        return cap_frame_cap_new(
-                   IA32_SmallPage,         /* capFSize             */
-                   0,                      /* capFIsIOSpace        */
-                   ASID_LOW(asidInvalid),  /* capFMappedASIDLow    */
-                   0,                      /* capFMappedAddress    */
-                   ASID_HIGH(asidInvalid), /* capFMappedASIDHigh   */
-                   VMReadWrite,            /* capFVMRights         */
-                   (word_t)regionBase      /* capFBasePtr          */
-               );
-
-    case seL4_IA32_LargePage:
-        memzero(regionBase, 1 << pageBitsForSize(IA32_LargePage));
-        return cap_frame_cap_new(
-                   IA32_LargePage,         /* capFSize             */
-                   0,                      /* capFIsIOSpace        */
-                   ASID_LOW(asidInvalid),  /* capFMappedASIDLow    */
-                   0,                      /* capFMappedAddress    */
-                   ASID_HIGH(asidInvalid), /* capFMappedASIDHigh   */
-                   VMReadWrite,            /* capFVMRights         */
-                   (word_t)regionBase      /* capFBasePtr          */
-               );
-
-    case seL4_IA32_PageTableObject:
-        memzero(regionBase, 1 << PT_SIZE_BITS);
-        return cap_page_table_cap_new(
-                   0,                  /* capPTIsMapped        */
-                   asidInvalid,        /* capPTMappedASID      */
-                   0,                  /* capPTMappedAddress   */
-                   (word_t)regionBase  /* capPTBasePtr         */
-               );
-
-    case seL4_IA32_PageDirectoryObject:
-        memzero(regionBase, 1 << PD_SIZE_BITS);
-#ifndef CONFIG_PAE_PAGING
-        copyGlobalMappings(regionBase);
-#endif
-        return cap_page_directory_cap_new(
-                   0,                  /* capPDIsMapped    */
-                   asidInvalid,        /* capPDMappedASID  */
-                   0,                  /* capPDMappedAddress */
-                   (word_t)regionBase  /* capPDBasePtr     */
-               );
-
-#ifdef CONFIG_PAE_PAGING
-    case seL4_IA32_PDPTObject:
-        memzero(regionBase, 1 << PDPT_SIZE_BITS);
-        copyGlobalMappings(regionBase);
-
-        return cap_pdpt_cap_new(
-                   0,                  /* capPDPTIsMapped */
-                   asidInvalid,        /* capPDPTMappedAsid*/
-                   (word_t)regionBase  /* capPDPTBasePtr */
-               );
-#endif
-
-    case seL4_IA32_IOPageTableObject:
-        memzero(regionBase, 1 << VTD_PT_SIZE_BITS);
-        return cap_io_page_table_cap_new(
-                   0,  /* capIOPTIsMapped      */
-                   0,  /* capIOPTLevel         */
-                   0,  /* capIOPTMappedAddress */
-                   0,  /* capIOPTIOASID        */
-                   (word_t)regionBase  /* capIOPTBasePtr */
-               );
-
-    default:
-        /*
-         * This is a conflation of the haskell error: "Arch.createNewCaps
-         * got an API type" and the case where an invalid object type is
-         * passed (which is impossible in haskell).
-         */
-        fail("Arch_createObject got an API type or invalid object type");
-    }
+    return Mode_createObject(t, regionBase, userSize);
 }
 
 exception_t
@@ -544,12 +430,6 @@ Arch_decodeInvocation(
 )
 {
     switch (cap_get_capType(cap)) {
-    case cap_pdpt_cap:
-    case cap_page_directory_cap:
-    case cap_page_table_cap:
-    case cap_frame_cap:
-        return decodeX86MMUInvocation(invLabel, length, cptr, slot, cap, excaps, buffer);
-
     case cap_asid_control_cap:
     case cap_asid_pool_cap:
         return decodeX86MMUInvocation(invLabel, length, cptr, slot, cap, excaps, buffer);
@@ -559,11 +439,8 @@ Arch_decodeInvocation(
         return decodeIA32IOSpaceInvocation(invLabel, cap);
     case cap_io_page_table_cap:
         return decodeIA32IOPTInvocation(invLabel, length, slot, cap, excaps, buffer);
-
     default:
-        current_syscall_error.type = seL4_InvalidCapability;
-        current_syscall_error.invalidCapNumber = 0;
-        return EXCEPTION_SYSCALL_ERROR;
+        return Mode_decodeInvocation(invLabel, length, cptr, slot, cap, excaps, buffer);
     }
 }
 
