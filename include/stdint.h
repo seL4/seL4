96d0167cd1287eedeeb7e2b5009057020fb5a00d
Anna Lyons <Anna.Lyons@nicta.com.au>
2015-12-02 14:30:42 +1100

    SELFOUR-324: add max values to stdint.h

--- include/stdint.h
+++ include/stdint.h
@@ -21,4 +21,9 @@ typedef signed short int16_t;
 typedef signed int int32_t;
 typedef signed long long int64_t;
 
+#define UINT64_MAX (0xFFFFFFFFFFFFFFFFULL)
+#define UINT32_MAX (0xFFFFFFFFU)
+#define INT64_MAX  (0x7FFFFFFFFFFFFFFFULL)
+#define INT32_MAX  (0x7FFFFFFU)
+
 #endif
