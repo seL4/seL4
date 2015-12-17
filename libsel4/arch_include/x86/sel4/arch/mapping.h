c2a85cdcb14b4b04619593261a49c310a8f59185
Adrian Danis <Adrian.Danis@nicta.com.au>
2015-12-17 14:02:22 +1100

    libsel4: Define different lookup levels for PAE

--- libsel4/arch_include/x86/sel4/arch/mapping.h
+++ libsel4/arch_include/x86/sel4/arch/mapping.h
@@ -11,8 +11,15 @@
 #ifndef __LIBSEL4_ARCH_MAPPING
 #define __LIBSEL4_ARCH_MAPPING
 
+#include <autoconf.h>
+
 #define SEL4_MAPPING_LOOKUP_LEVEL 2
+#ifdef CONFIG_PAE_PAGING
+#define SEL4_MAPPING_LOOKUP_NO_PT 21
+#define SEL4_MAPPING_LOOKUP_NO_PD 30
+#else
 #define SEL4_MAPPING_LOOKUP_NO_PT 22
+#endif
 
 static inline seL4_Word seL4_MappingFailedLookupLevel()
 {
