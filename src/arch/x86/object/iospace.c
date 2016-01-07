c3b04449d77e11387b9ae7f543a482b9d8e4a3dc
Adrian Danis <Adrian.Danis@nicta.com.au>
2016-01-07 15:38:56 +1100

    x86: Remove nonexistant iospace syscalls

--- src/arch/x86/object/iospace.c
+++ src/arch/x86/object/iospace.c
@@ -454,15 +454,7 @@ decodeIA32IOUnMapInvocation(
 
 exception_t decodeIA32IOSpaceInvocation(word_t invLabel, cap_t cap)
 {
-    vtd_cte_t *cte;
-    if (invLabel != IA32IOSpaceRemovePassthrough) {
-        current_syscall_error.type = seL4_IllegalOperation;
-        return EXCEPTION_SYSCALL_ERROR;
-    }
-    cte = lookup_vtd_context_slot(cap);
-    *cte = vtd_cte_set_translation_type(*cte, 0);
-    flushCacheRange(cte, VTD_CTE_SIZE_BITS);
-    invalidate_iotlb();
-    setThreadState(ksCurThread, ThreadState_Restart);
-    return EXCEPTION_NONE;
+    userError("IOSpace capability has no invocations");
+    current_syscall_error.type = seL4_IllegalOperation;
+    return EXCEPTION_SYSCALL_ERROR;
 }
