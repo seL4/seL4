30e5b5948d577f05f8689bf3068d7f7a7457a5da
Adrian Danis <Adrian.Danis@nicta.com.au>
2016-02-10 17:01:57 +1100

    trivial: style

--- libsel4/sel4_arch_include/ia32/sel4/sel4_arch/deprecated.h
+++ libsel4/sel4_arch_include/ia32/sel4/sel4_arch/deprecated.h
@@ -46,98 +46,117 @@ typedef seL4_X86_PageTable seL4_IA32_PageTable IA32_DEPRECATED;
 typedef seL4_X86_IOPageTable seL4_IA32_IOPageTable IA32_DEPRECATED;
 
 static inline int IA32_DEPRECATED
-seL4_IA32_PageDirectory_Map(seL4_X86_PageDirectory service, seL4_CPtr vroot, seL4_Word vaddr, seL4_X86_VMAttributes attr) {
+seL4_IA32_PageDirectory_Map(seL4_X86_PageDirectory service, seL4_CPtr vroot, seL4_Word vaddr, seL4_X86_VMAttributes attr)
+{
     return seL4_X86_PageDirectory_Map(service, vroot, vaddr, attr);
 }
 
 static inline int IA32_DEPRECATED
-seL4_IA32_PageDirectory_Unmap(seL4_X86_PageDirectory service) {
+seL4_IA32_PageDirectory_Unmap(seL4_X86_PageDirectory service)
+{
     return seL4_X86_PageDirectory_Unmap(service);
 }
 
 static inline int IA32_DEPRECATED
-seL4_IA32_PageTable_Map(seL4_X86_PageTable service, seL4_CPtr vroot, seL4_Word vaddr, seL4_X86_VMAttributes attr) {
+seL4_IA32_PageTable_Map(seL4_X86_PageTable service, seL4_CPtr vroot, seL4_Word vaddr, seL4_X86_VMAttributes attr)
+{
     return seL4_X86_PageTable_Map(service, vroot, vaddr, attr);
 }
 
 static inline int IA32_DEPRECATED
-seL4_IA32_PageTable_Unmap(seL4_X86_PageTable service) {
+seL4_IA32_PageTable_Unmap(seL4_X86_PageTable service)
+{
     return seL4_X86_PageTable_Unmap(service);
 }
 
 #ifdef CONFIG_IOMMU
 static inline int IA32_DEPRECATED
-seL4_IA32_IOPageTable_Map(seL4_X86_IOPageTable service, seL4_X86_IOSpace iospace, seL4_Word ioaddr) {
+seL4_IA32_IOPageTable_Map(seL4_X86_IOPageTable service, seL4_X86_IOSpace iospace, seL4_Word ioaddr)
+{
     return seL4_X86_IOPageTable_Map(service, iospace, ioaddr);
 }
 
 static inline int IA32_DEPRECATED
-seL4_IA32_IOPageTable_Unmap(seL4_X86_IOPageTable service) {
+seL4_IA32_IOPageTable_Unmap(seL4_X86_IOPageTable service)
+{
     return seL4_X86_IOPageTable_Unmap(service);
 }
 
 static inline int IA32_DEPRECATED
-seL4_IA32_Page_MapIO(seL4_X86_Page service, seL4_X86_IOSpace iospace, seL4_CapRights rights, seL4_Word ioaddr) {
+seL4_IA32_Page_MapIO(seL4_X86_Page service, seL4_X86_IOSpace iospace, seL4_CapRights rights, seL4_Word ioaddr)
+{
     return seL4_X86_Page_MapIO(service, iospace, rights, ioaddr);
 }
 #endif
 
 static inline int IA32_DEPRECATED
-seL4_IA32_Page_Map(seL4_X86_Page service, seL4_CPtr vroot, seL4_Word vaddr, seL4_CapRights rights, seL4_X86_VMAttributes attr) {
+seL4_IA32_Page_Map(seL4_X86_Page service, seL4_CPtr vroot, seL4_Word vaddr, seL4_CapRights rights, seL4_X86_VMAttributes attr)
+{
     return seL4_X86_Page_Map(service, vroot, vaddr, rights, attr);
 }
 
 static inline int IA32_DEPRECATED
-seL4_IA32_Page_Remap(seL4_X86_Page service, seL4_CPtr vroot, seL4_CapRights rights, seL4_X86_VMAttributes attr) {
+seL4_IA32_Page_Remap(seL4_X86_Page service, seL4_CPtr vroot, seL4_CapRights rights, seL4_X86_VMAttributes attr)
+{
     return seL4_X86_Page_Remap(service, vroot, rights, attr);
 }
 
 static inline int IA32_DEPRECATED
-seL4_IA32_Page_Unmap(seL4_X86_Page service) {
+seL4_IA32_Page_Unmap(seL4_X86_Page service)
+{
     return seL4_X86_Page_Unmap(service);
 }
 
 static inline seL4_X86_Page_GetAddress_t IA32_DEPRECATED
-seL4_IA32_Page_GetAddress(seL4_X86_Page service) {
+seL4_IA32_Page_GetAddress(seL4_X86_Page service)
+{
     return seL4_X86_Page_GetAddress(service);
 }
 
 static inline int IA32_DEPRECATED
-seL4_IA32_ASIDControl_MakePool(seL4_X86_ASIDControl service, seL4_Untyped untyped, seL4_CNode root, seL4_Word index, seL4_Uint8 depth) {
+seL4_IA32_ASIDControl_MakePool(seL4_X86_ASIDControl service, seL4_Untyped untyped, seL4_CNode root, seL4_Word index, seL4_Uint8 depth)
+{
     return seL4_X86_ASIDControl_MakePool(service, untyped, root, index, depth);
 }
 
 static inline int IA32_DEPRECATED
-seL4_IA32_ASIDPool_Assign(seL4_X86_ASIDPool service, seL4_CPtr vroot) {
+seL4_IA32_ASIDPool_Assign(seL4_X86_ASIDPool service, seL4_CPtr vroot)
+{
     return seL4_X86_ASIDPool_Assign(service, vroot);
 }
 
 static inline seL4_X86_IOPort_In8_t IA32_DEPRECATED
-seL4_IA32_IOPort_In8(seL4_X86_IOPort service, seL4_Uint16 port) {
+seL4_IA32_IOPort_In8(seL4_X86_IOPort service, seL4_Uint16 port)
+{
     return seL4_X86_IOPort_In8(service, port);
 }
 
 static inline seL4_X86_IOPort_In16_t IA32_DEPRECATED
-seL4_IA32_IOPort_In16(seL4_X86_IOPort service, seL4_Uint16 port) {
+seL4_IA32_IOPort_In16(seL4_X86_IOPort service, seL4_Uint16 port)
+{
     return seL4_X86_IOPort_In16(service, port);
 }
 
 static inline seL4_X86_IOPort_In32_t IA32_DEPRECATED
-seL4_IA32_IOPort_In32(seL4_X86_IOPort service, seL4_Uint16 port) {
+seL4_IA32_IOPort_In32(seL4_X86_IOPort service, seL4_Uint16 port)
+{
     return seL4_X86_IOPort_In32(service, port);
 }
 
 static inline int IA32_DEPRECATED
-seL4_IA32_IOPort_Out8(seL4_X86_IOPort service, seL4_Word port, seL4_Word data) {
+seL4_IA32_IOPort_Out8(seL4_X86_IOPort service, seL4_Word port, seL4_Word data)
+{
     return seL4_X86_IOPort_Out8(service, port, data);
 }
 static inline int IA32_DEPRECATED
-seL4_IA32_IOPort_Out16(seL4_X86_IOPort service, seL4_Word port, seL4_Word data) {
+seL4_IA32_IOPort_Out16(seL4_X86_IOPort service, seL4_Word port, seL4_Word data)
+{
     return seL4_X86_IOPort_Out16(service, port, data);
 }
 
 static inline int IA32_DEPRECATED
-seL4_IA32_IOPort_Out32(seL4_X86_IOPort service, seL4_Word port, seL4_Word data) {
+seL4_IA32_IOPort_Out32(seL4_X86_IOPort service, seL4_Word port, seL4_Word data)
+{
     return seL4_X86_IOPort_Out32(service, port, data);
 }
 
