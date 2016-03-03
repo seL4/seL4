1cdc450cb38896db9ededeaa888bb1ab2f62bdd1
Yanyan Shen <yanyan.shen@nicta.com.au>
2016-03-03 15:57:13 +1100

    arm_hyp: remove unused function declaration.
    remove map_hypervisor_devices function declaration.

--- include/plat/apq8064/plat/machine/hardware.h
+++ include/plat/apq8064/plat/machine/hardware.h
@@ -65,9 +65,6 @@ p_region_t get_avail_p_reg(word_t i);
 int get_num_dev_p_regs(void);
 p_region_t get_dev_p_reg(word_t i);
 void map_kernel_devices(void);
-#ifdef ARM_HYP
-void map_hypervisor_devices(void);
-#endif /* ARM_HYP */
 
 bool_t CONST isReservedIRQ(irq_t irq);
 void handleReservedIRQ(irq_t irq);
