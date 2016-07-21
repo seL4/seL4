d1153e71cb1e257b3020604318137f7e49ce6381
Anna Lyons <Anna.Lyons@nicta.com.au>
2016-07-21 10:22:03 +1000

    Add seL4_TemporaFaultReply_new

--- libsel4/include/sel4/faults.h
+++ libsel4/include/sel4/faults.h
@@ -63,4 +63,16 @@ seL4_isCapFault_tag(seL4_MessageInfo_t tag)
     return seL4_MessageInfo_get_label(tag) == seL4_Fault_CapFault;
 }
 
+static inline seL4_MessageInfo_t
+seL4_TemporalFaultReply_new(seL4_Bool resume, seL4_UserContext regs, seL4_Word length)
+{
+    seL4_MessageInfo_t info = seL4_MessageInfo_new(!resume, 0, 0, length);
+
+    for (seL4_Word i = 0; i < length; i++) {
+        seL4_SetMR(i, ((seL4_Word *) &regs)[i]);
+    }
+
+    return info;
+}
+
 #endif
