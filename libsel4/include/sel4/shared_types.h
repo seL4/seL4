55c3fcd5a3c84665f7421a2666a92a8658658790
Stephen Sherratt <Stephen.Sherratt@data61.csiro.au>
2016-08-15 11:20:38 +1000

    libsel4: Removed erroneous semicolon

--- libsel4/include/sel4/shared_types.h
+++ libsel4/include/sel4/shared_types.h
@@ -39,6 +39,6 @@ enum {
 #define seL4_CanRead   seL4_CapRights_new(false, true, false)
 #define seL4_CanWrite  seL4_CapRights_new(false, false, true)
 #define seL4_CanGrant  seL4_CapRights_new(true, false, false)
-#define seL4_NoWrite   seL4_CapRights_new(true, true, false);
+#define seL4_NoWrite   seL4_CapRights_new(true, true, false)
 
 #endif
