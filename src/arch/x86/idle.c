394994c2f18a1a659d767231134912228a977dbe
amrzar <azarrabi@nicta.com.au>
2016-10-12 11:28:16 +1100

    x86: replace idle/halt[.S] with idle.c

---
+++ src/arch/x86/idle.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2016, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(D61_GPL)
+ */
+
+#include <config.h>
+
+/** DONT_TRANSLATE */
+void idle_thread(void)
+{
+    while (1) {
+        asm volatile("hlt");
+    }
+}
+
+/** DONT_TRANSLATE */
+void VISIBLE halt(void)
+{
+    /* halt is actually, idle thread without the interrupts */
+    asm volatile("cli");
+
+#ifdef CONFIG_PRINTING
+    printf("halting...");
+#endif
+    idle_thread();
+    UNREACHABLE();
+}
