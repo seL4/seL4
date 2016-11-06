1173e19aa6f96b1bf4e782ab12d1677185dd2821
Adrian Danis <Adrian.Danis@data61.csiro.au>
2016-11-07 10:37:38 +1100

    x86: kernelSP only needed for ia32

--- include/arch/x86/arch/machine/registerset.h
+++ include/arch/x86/arch/machine/registerset.h
@@ -68,7 +68,7 @@ typedef struct user_fpu_state user_fpu_state_t;
 struct user_context {
     user_fpu_state_t fpuState;
     word_t registers[n_contextRegisters];
-#if CONFIG_MAX_NUM_NODES > 1
+#if CONFIG_MAX_NUM_NODES > 1 && defined(CONFIG_ARCH_IA32)
     /* stored pointer to kernel stack used when kernel run in current TCB context. */
     word_t kernelSP;
 #endif
@@ -86,7 +86,7 @@ word_t sanitiseRegister(register_t reg, word_t v);
 compile_assert(fpu_state_alignment_valid,
                OFFSETOF(user_context_t, fpuState) % MIN_FPU_ALIGNMENT == 0)
 
-#if CONFIG_MAX_NUM_NODES > 1
+#if CONFIG_MAX_NUM_NODES > 1 && defined(CONFIG_ARCH_IA32)
 /* Ensure kernelSP is the first member following the registers. */
 compile_assert(kernelSP_alignment_valid,
                OFFSETOF(user_context_t, kernelSP) -
