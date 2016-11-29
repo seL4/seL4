818a8330b33b0086f9b4679468eac00033eeff34
Stephen Sherratt <Stephen.Sherratt@data61.csiro.au>
2016-11-29 12:18:34 +1100

    manual: Added libsel4 tools library to manual dir
    JIRA: SELFOUR-606

---
+++ libsel4/tools/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#
+# Copyright 2016, Data 61
+# Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+# ABN 41 687 119 230.
+#
+# This software may be distributed and modified according to the terms of
+# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+# See "LICENSE_BSD2.txt" for details.
+#
+# @TAG(D61_BSD)
+#
+
+# This file allows the the libsel4 tools directory to be imported as
+# a python module.
+
+import syscall_stub_gen
