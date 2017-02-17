e0e49db0ece32af40cdcc5dd81d210078bf2b1d9
Hesham Almatary <hesham.almatary@data61.csiro.au>
2017-02-17 11:23:49 +1100

    trivial: add place holder smp.h file for aarch64

---
+++ include/arch/arm/arch/64/mode/smp/smp.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(D61_GPL)
+ */
+
+#ifndef __MODE_SMP_H_
+#define __MODE_SMP_H_
+
+#include <config.h>
+
+#if CONFIG_MAX_NUM_NODES > 1
+
+static inline CONST cpu_id_t
+getCurrentCPUIndex(void)
+{
+    /* TODO: currently only supports one core */
+#warning "getCurrentCPUIndex is not implemented yet"
+    return 0;
+}
+
+#endif /* CONFIG_MAX_NUM_NODES > 1 */
+
+#endif /* __MODE_SMP_H_ */
