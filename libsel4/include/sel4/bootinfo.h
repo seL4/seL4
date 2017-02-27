099b45074f24533133e7a862982e65b5e2e721a4
Adrian Danis <Adrian.Danis@data61.csiro.au>
2017-02-27 14:10:47 +1100

    libsel4: Deprecate bootinfo management

--- libsel4/include/sel4/bootinfo.h
+++ libsel4/include/sel4/bootinfo.h
@@ -13,8 +13,9 @@
 
 #include <sel4/types.h>
 #include <sel4/bootinfo_types.h>
+#include <sel4/macros.h>
 
-void seL4_InitBootInfo(seL4_BootInfo* bi);
-seL4_BootInfo* seL4_GetBootInfo(void);
+void seL4_InitBootInfo(seL4_BootInfo* bi) SEL4_DEPRECATED("libsel4 management of bootinfo is deprecated, see the BootInfo Frame section of the manual");
+seL4_BootInfo* seL4_GetBootInfo(void) SEL4_DEPRECATED("libsel4 management of bootinfo is deprecated, see the BootInfo Frame section of the manual");
 
 #endif // __LIBSEL4_BOOTINFO_H
