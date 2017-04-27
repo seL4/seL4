30c3862ce85f53558212943ee10307a6c9d6cb3e
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-04-27 15:03:24 +1000

    inline checkBudget and checkBudgetRestart
    These are called on the syscall hot path

--- include/kernel/thread.h
+++ include/kernel/thread.h
@@ -157,6 +157,12 @@ void rescheduleRequired(void);
  * registers into their correct place */
 void Arch_postModifyRegisters(tcb_t *tptr);
 
+/* End the timeslice for the current thread.
+ * This will recharge the threads timeslice and place it at the
+ * end of the scheduling queue for its priority.
+ */
+void endTimeslice(void);
+
 /* Update the kernels timestamp and stores in ksCurTime.
  * The difference between the previous kernel timestamp and the one just read
  * is stored in ksConsumed.
@@ -200,25 +206,60 @@ updateTimestamp(bool_t incrementConsumedTime)
  * @return true if the thread/domain has enough budget to
  *              get through the current kernel operation.
  */
-bool_t checkBudget(void);
+static inline bool_t
+checkBudget(void) {
+    /* currently running thread must have available capacity */
+    assert(refill_ready(NODE_STATE(ksCurSC)));
+
+    if (unlikely(NODE_STATE(ksCurThread) == NODE_STATE(ksIdleThread))) {
+        return true;
+    }
+
+    ticks_t capacity = refill_capacity(NODE_STATE(ksCurSC), NODE_STATE(ksConsumed));
+    if (unlikely(capacity < MIN_BUDGET)) {
+        if (capacity == 0) {
+            NODE_STATE(ksConsumed) = refill_budget_check(NODE_STATE(ksCurSC), NODE_STATE(ksConsumed));
+        }
+        if (NODE_STATE(ksConsumed) > 0) {
+            refill_split_check(NODE_STATE(ksCurSC), NODE_STATE(ksConsumed));
+        }
+        NODE_STATE(ksConsumed) = 0;
+        NODE_STATE(ksCurTime) += 1llu;
+        if (likely(isRunnable(NODE_STATE(ksCurThread)))) {
+            endTimeslice();
+            rescheduleRequired();
+        }
+        return false;
+    } else if (unlikely(isCurDomainExpired())) {
+        commitTime();
+        rescheduleRequired();
+        return false;
+    }
+    return true;
+}
 
 /* Everything checkBudget does, but also set the thread
  * state to ThreadState_Restart. To be called from kernel entries
  * where the operation should be restarted once the current thread
  * has budget again.
  */
-bool_t checkBudgetRestart(void);
+
+static inline bool_t
+checkBudgetRestart(void)
+{
+    assert(isRunnable(NODE_STATE(ksCurThread)));
+    bool_t result = checkBudget();
+    if (!result) {
+        setThreadState(NODE_STATE(ksCurThread), ThreadState_Restart);
+    }
+    return result;
+}
+
 
 /* Set the next kernel tick, which is either the end of the current
  * domains timeslice OR the end of the current threads timeslice.
  */
 void setNextInterrupt(void);
-
-/* End the timeslice for the current thread.
- * This will recharge the threads timeslice and place it at the
- * end of the scheduling queue for its priority.
- */
-void endTimeslice(void);
 
 /* Wake any periodic threads that are ready for budget recharge */
 void awaken(void);
