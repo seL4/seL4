56e9e28d493001f3a1aaee020c5090779b904707
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2017-05-15 15:29:21 +1000

    libsel4: add comments to multiboot fields

--- libsel4/arch_include/x86/sel4/arch/bootinfo_types.h
+++ libsel4/arch_include/x86/sel4/arch/bootinfo_types.h
@@ -98,11 +98,15 @@ typedef struct _seL4_X86_BootInfo_VBE {
     seL4_Uint32 vbeInterfaceLen;
 } SEL4_PACKED seL4_X86_BootInfo_VBE;
 
+/**
+ * Copy of multiboot mmap fields.
+ * https://www.gnu.org/software/grub/manual/multiboot/multiboot.html
+ */
 typedef struct seL4_X86_mb_mmap {
-    uint32_t size;
-    uint64_t base_addr;
-    uint64_t length;
-    uint32_t type;
+    uint32_t size; // size of this struct in bytes
+    uint64_t base_addr; // physical address of start of this region
+    uint64_t length; // length of the region this struct represents in bytes
+    uint32_t type; // memory type of region. Type 1 corresponds to RAM.
 } SEL4_PACKED seL4_X86_mb_mmap_t;
 
 typedef struct seL4_X86_BootInfo_mmap {
