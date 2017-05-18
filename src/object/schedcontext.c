cf5d396d49c0e5c497e5c0ab119293e4a51305a8
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-05-18 15:11:43 +1000

    VER-789: Don't put empty sc's in the scheduler

--- src/object/schedcontext.c
+++ src/object/schedcontext.c
@@ -194,7 +194,7 @@ schedContext_bindTCB(sched_context_t *sc, tcb_t *tcb)
 #endif
 
     schedContext_resume(sc);
-    if (isSchedulable(tcb)) {
+    if (isSchedulable(tcb) && sc->scRefillMax > 0) {
         switchIfRequiredTo(tcb);
     }
 }
