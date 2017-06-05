7333e97c65c9bf6c3545fc45eb5c4e424d3f8994
Stephen Sherratt <Stephen.Sherratt@data61.csiro.au>
2017-06-05 15:46:09 +1000

    Add pip metapackage for downloading dependencies
    Dependencies can now be downloaded with:
    pip install sel4-deps
    
    Dependencies can be changed by modifying the setup.py script.
    
    Information on how to update the package is in the setup.py script.
    
    JIRA: SELFOUR-909

---
+++ tools/python-deps/README.rst
@@ -0,0 +1,5 @@
+seL4 Python Dependency Metapackage
+##################################
+
+An empty package depending on all the python packages required to build the seL4
+microkernel, its manual, and proofs.
---
+++ tools/python-deps/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# Copyright 2017, Data 61
+# Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+# ABN 41 687 119 230.
+#
+# This software may be distributed and modified according to the terms of
+# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+# See "LICENSE_BSD2.txt" for details.
+#
+# @TAG(DATA61_BSD)
+#
+
+"""
+Setup script for dependency metapackage.
+
+To add a python dependency, add it to the DEPS list below.
+
+To publish to pypitest:
+python setup.py sdist upload -r pypitest
+
+To publish to pypi:
+python setup.py sdist upload -r pypi
+"""
+
+from setuptools import setup
+
+DEPS = [
+    'six',
+    'future',
+    'tempita',
+    'lxml',
+    'ply',
+    'psutil',
+]
+
+setup(
+    name='sel4-deps',
+    version='0.1.0',
+    description='Metapackage for downloading build dependencies for the seL4 microkernel',
+    url="https://sel4.systems",
+    licence='BSD2',
+    author='TrustworthySystems',
+    author_email='Stephen.Sherratt@data61.csiro.au',
+    install_requires=DEPS,
+)
