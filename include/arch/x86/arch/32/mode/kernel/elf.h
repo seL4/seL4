40c61e5c18dc850b80398b5d39ff00e82d64a2ce
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-06-19 11:29:44 +1000

    Fix licenses (the rest)

--- include/arch/x86/arch/32/mode/kernel/elf.h
+++ include/arch/x86/arch/32/mode/kernel/elf.h
@@ -1,7 +1,7 @@
 /*
- * Copyright 2016, Data61
+ * Copyright 2017, Data61
  * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
- * ABN 41 687 119 230
+ * ABN 41 687 119 230.
  *
  * This software may be distributed and modified according to the terms of
  * the GNU General Public License version 2. Note that NO WARRANTY is provided.
--- src/arch/arm/c_traps.c
+++ src/arch/arm/c_traps.c
@@ -1,5 +1,7 @@
 /*
- * Copyright 2016, Data61 CSIRO
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
  *
  * This software may be distributed and modified according to the terms of
  * the GNU General Public License version 2. Note that NO WARRANTY is provided.
