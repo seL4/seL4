9541a3ced46055e5877342b0bd8f9819497e610a
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-06-22 13:47:22 +1000

    Merge seL4_DebugDumpScheduler
    - libsel4: update seL4_DebugDumpScheduler calls
    - show release queue state in output

--- libsel4/sel4_arch_include/x86_64/sel4/sel4_arch/syscalls.h
+++ libsel4/sel4_arch_include/x86_64/sel4/sel4_arch/syscalls.h
@@ -567,7 +567,7 @@ seL4_DebugDumpScheduler(void)
     seL4_Word unused4 = 0;
     seL4_Word unused5 = 0;
 
-    x64_sys_send_recv(seL4_SysDebugDumpScheduler, 0, &unused0, 0, &unused1, &unused2, &unused3, &unused4, &unused5);
+    x64_sys_send_recv(seL4_SysDebugDumpScheduler, 0, &unused0, 0, &unused1, &unused2, &unused3, &unused4, &unused5, 0);
 }
 #endif
 
