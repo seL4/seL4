56e5339039fa51124f0c83f323fd3d83db4cc45b
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-07-11 16:37:17 +1000

    global_timer.h: disable compare irq in ackInterrupt
    This prevents timer irqs coming in twice on the sabre.
    
    What was happening
    
    - timer irqs would arrive
    - we'd ack the irq in the global timer and the GIC
    - then we'd reprogram the timer
    
    However the compare flag was left on and once the irq was acked it would
    trigger again. The solution is to turn off the compare flag when acking
    the timer irq.

--- include/arch/arm/arch/machine/global_timer.h
+++ include/arch/arm/arch/machine/global_timer.h
@@ -90,6 +90,8 @@ setDeadline(ticks_t deadline)
 static inline void
 ackDeadlineIRQ(void)
 {
+    /* disable cmp */
+    globalTimer->control &= ~(BIT(COMP_ENABLE));
     /* ack the isr */
     globalTimer->isr = 1;
 }
