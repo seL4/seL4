f0125a1f81418fd53d8df59fe368f23898ca4d73
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-10-12 16:09:03 +1100

    Fix broken syscall_sysenter stubs

--- libsel4/sel4_arch_include/x86_64/sel4/sel4_arch/syscalls_sysenter.h
+++ libsel4/sel4_arch_include/x86_64/sel4/sel4_arch/syscalls_sysenter.h
@@ -22,6 +22,8 @@ x64_sys_send(seL4_Word sys, seL4_Word dest, seL4_Word info, seL4_Word msg0, seL4
 {
     register seL4_Word mr0 asm("r10") = msg0;
     register seL4_Word mr1 asm("r8") = msg1;
+    register seL4_Word mr2 asm("r9") = msg2;
+    register seL4_Word mr3 asm("r15") = msg3;
 
     asm volatile (
         "movq   %%rsp, %%rcx        \n"
@@ -57,7 +59,7 @@ x64_sys_send_null(seL4_Word sys, seL4_Word dest, seL4_Word info)
 }
 
 static inline void
-x64_sys_recv(seL4_Word sys, seL4_Word src, seL4_Word *out_badge, seL4_Word *out_info, seL4_Word *out_mr0, seL4_Word *out_mr1, seL4_Word msg2, seL4_Word msg3, seL4_Word reply)
+x64_sys_recv(seL4_Word sys, seL4_Word src, seL4_Word *out_badge, seL4_Word *out_info, seL4_Word *out_mr0, seL4_Word *out_mr1, seL4_Word *out_mr2, seL4_Word *out_mr3, seL4_Word reply)
 {
     register seL4_Word mr0 asm("r10");
     register seL4_Word mr1 asm("r8");
@@ -115,7 +117,7 @@ x64_sys_send_recv(seL4_Word sys, seL4_Word dest, seL4_Word *out_dest, seL4_Word 
         "r" (mr1),
         "r" (mr2),
         "r" (mr3),
-        "r" (reply)
+        "r" (reply_reg)
         : "%rcx", "%rdx", "memory"
     );
 
@@ -126,10 +128,12 @@ x64_sys_send_recv(seL4_Word sys, seL4_Word dest, seL4_Word *out_dest, seL4_Word 
 }
 
 static inline void
-x64_sys_nbsend_recv(seL4_Word sys, seL4_Word dest, seL4_Word src, seL4_Word *out_dest, seL4_Word info, seL4_Word *out_info, seL4_Word *in_out_mr0, seL4_Word *in_out_mr1, seL4_Word reply)
+x64_sys_nbsend_recv(seL4_Word sys, seL4_Word dest, seL4_Word src, seL4_Word *out_dest, seL4_Word info, seL4_Word *out_info, seL4_Word *in_out_mr0, seL4_Word *in_out_mr1, seL4_Word *in_out_mr2, seL4_Word *in_out_mr3, seL4_Word reply)
 {
     register seL4_Word mr0 asm("r10") = *in_out_mr0;
     register seL4_Word mr1 asm("r8") = *in_out_mr1;
+    register seL4_Word mr2 asm("r9") = *in_out_mr2;
+    register seL4_Word mr3 asm("r15") = *in_out_mr3;
     register seL4_Word reply_reg asm("r12") = reply;
     register seL4_Word dest_reg asm("r13") = dest;
 
@@ -154,6 +158,8 @@ x64_sys_nbsend_recv(seL4_Word sys, seL4_Word dest, seL4_Word src, seL4_Word *out
 
     *in_out_mr0 = mr0;
     *in_out_mr1 = mr1;
+    *in_out_mr2 = mr2;
+    *in_out_mr3 = mr3;
 }
 
 static inline void
