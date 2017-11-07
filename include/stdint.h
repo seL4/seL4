4b2114782c82ac2616bac8a00ef19f8cb6c37457
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-11-07 14:52:04 +1100

    Add stdint MAX definitions
    - UINTPTR_MAX
    - UINT64_MAX
    - UINT32_MAX
    - INT64_MAX
    - INT32_MAX

--- include/stdint.h
+++ include/stdint.h
@@ -11,6 +11,8 @@
 #ifndef __STDINT_H
 #define __STDINT_H
 
+#include <mode/stdint.h>
+
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
@@ -20,5 +22,10 @@ typedef signed char int8_t;
 typedef signed short int16_t;
 typedef signed int int32_t;
 typedef signed long long int64_t;
+
+#define UINT64_MAX (0xFFFFFFFFFFFFFFFF)
+#define UINT32_MAX (0xFFFFFFFF)
+#define INT64_MAX  (0x7FFFFFFFFFFFFFFF)
+#define INT32_MAX  (0x7FFFFFFF)
 
 #endif
