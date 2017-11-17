cb814780159a5c4c6062251a59767e43c26f7342
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-11-17 14:58:29 +1100

    aarch64: add constants for generic timer
    This brings the aarch64 generic timer into exactly the same code as
    the aarch32 generic timer.

--- include/arch/arm/arch/64/mode/machine/timer.h
+++ include/arch/arm/arch/64/mode/machine/timer.h
@@ -23,13 +23,17 @@
 /* but multiply by timer tick ms */
 #define TIMER_RELOAD    (TICKS_PER_MS * CONFIG_TIMER_TICK_MS)
 
+#define CNT_TVAL "cntv_tval_el0"
+#define CNT_CTL  "cntv_ctl_el0"
+#define CNTFRQ   "cntfrq_el0"
+
 /* ARM generic timer implementation */
 
 static inline void
 resetGenericTimer(void)
 {
-    SYSTEM_WRITE_WORD("cntv_tval_el0", TIMER_RELOAD);
-    SYSTEM_WRITE_WORD("cntv_ctl_el0", BIT(0));
+    SYSTEM_WRITE_WORD(CNT_TVAL, TIMER_RELOAD);
+    SYSTEM_WRITE_WORD(CNT_CTL, BIT(0));
 }
 
 BOOT_CODE static inline void
@@ -38,7 +42,7 @@ initGenericTimer(void)
     if (config_set(CONFIG_DEBUG_BUILD)) {
         /* check the frequency is correct */
         uint32_t gpt_cnt_tval;
-        SYSTEM_READ_WORD("cntfrq_el0", gpt_cnt_tval);
+        SYSTEM_READ_WORD(CNTFRQ, gpt_cnt_tval);
         if (gpt_cnt_tval != 0 && gpt_cnt_tval != TIMER_CLOCK_HZ) {
             printf("Warning:  gpt_cnt_tval %u, expected %u\n", gpt_cnt_tval,
                    (uint32_t) TIMER_CLOCK_HZ);
