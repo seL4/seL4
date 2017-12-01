f380924e4ec2aab986eb701dcbd4b27c12a35d60
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-12-01 14:45:46 +1100

    Refactor replies to solve revoke problems.
    Before this change, we set the replyObject in the thread state on recv
    with no back pointer such that stray pointers would be left in the
    thread state when a reply object was completed.
    
    The new semantics are clearer and fix this problem by doing the
    following:
    
    - tcb->tcbReply is removed and the thread state field is always used,
    this was unneccessary duplication previously
    - the thread state value is set to the reply object only when the thread
    is in BlockedOnReply or BlockedOnRecv
    - the reply contains a back pointer, replyTCB, which points to that
    thread
    - if a thread has its reply removed, it must be set to
    ThreadState_Inactive.
    - deletion is easy in the blockedOnRecv case, we just unlink the reply
    and the tcb.
    - deletion is complicated for blockedOnReply. If we are deleing a tcb, we remove the actual
    reply object and the call chain is broken. If we are deleting a reply,
    we maintain the call chain by moving the tcb to the next reply.
    - we refactor the reply object interface to solve the above.
        * reply_clear: removes the reply from any connections (tcb, sc)
        * reply_unlink: just unlinks the tcb and reply, and sets the thread
        state to inactive
        * reply_remove: removes the reply from the call chain
        * reply_remove_tcb: removes the exact reply that a tcb is bound to,
        as we are removing that tcb. Breaks the call chain.

--- src/object/reply.c
+++ src/object/reply.c
@@ -18,13 +18,7 @@ reply_push(tcb_t *tcb_caller, tcb_t *tcb_callee, reply_t *reply, bool_t canDonat
 
     assert(tcb_caller != NULL);
     assert(reply != NULL);
-
-    if (unlikely(reply->replyCaller)) {
-        /* this reply should not be in an existing call stack - but it is
-         * this could be intentional but warn the user in debug mode anyway */
-        userError("Reply object already has unexecuted reply! Invoke first.");
-        reply_remove(reply);
-    }
+    assert(reply->replyTCB == NULL);
 
     if (tcb_callee->tcbSchedContext) {
         /* receiver already has sc */
@@ -33,14 +27,18 @@ reply_push(tcb_t *tcb_caller, tcb_t *tcb_callee, reply_t *reply, bool_t canDonat
 
     assert(call_stack_get_callStackPtr(reply->replyPrev) == 0);
     assert(call_stack_get_callStackPtr(reply->replyNext) == 0);
-    assert(reply->replyCaller == NULL);
 
     /* tcb caller should not be in a existing call stack */
-    assert(tcb_caller->tcbReply == NULL);
+    assert(thread_state_get_replyObject(tcb_caller->tcbState) == 0);
 
-    /* link tcb and reply */
-    reply->replyCaller = tcb_caller;
-    tcb_caller->tcbReply = reply;
+    /* unlink callee and reply - they may not have been linked already,
+     * if this rendesvous is occuring when seL4_Recv is called,
+     * however, no harm in overring 0 with 0 */
+    thread_state_ptr_set_replyObject(&tcb_callee->tcbState, 0);
+
+    /* link caller and reply */
+    reply->replyTCB = tcb_caller;
+    thread_state_ptr_set_replyObject(&tcb_caller->tcbState, REPLY_REF(reply));
     setThreadState(tcb_caller, ThreadState_BlockedOnReply);
 
     if (sc_donated != NULL && canDonate) {
@@ -70,12 +68,11 @@ void
 reply_pop(reply_t *reply)
 {
     assert(reply != NULL);
-    assert(reply->replyCaller != NULL);
-
+    assert(reply->replyTCB != NULL);
+    assert(thread_state_get_tsType(reply->replyTCB->tcbState) == ThreadState_BlockedOnReply);
     /* unlink tcb and reply */
-    tcb_t *tcb = reply->replyCaller;
-    reply->replyCaller = NULL;
-    tcb->tcbReply = NULL;
+    tcb_t *tcb = reply->replyTCB;
+    reply_unlink(reply);
 
     word_t next_ptr = call_stack_get_callStackPtr(reply->replyNext);
     word_t prev_ptr = call_stack_get_callStackPtr(reply->replyPrev);
@@ -89,6 +86,7 @@ reply_pop(reply_t *reply)
         SC_PTR(next_ptr)->scReply = REPLY_PTR(prev_ptr);
         if (prev_ptr != 0) {
             REPLY_PTR(prev_ptr)->replyNext = reply->replyNext;
+            assert(call_stack_get_isHead(REPLY_PTR(prev_ptr)->replyNext));
         }
 
         reply->replyPrev = call_stack_new(0, false);
@@ -100,6 +98,8 @@ reply_pop(reply_t *reply)
 void
 reply_remove(reply_t *reply)
 {
+    assert(thread_state_get_tsType(reply->replyTCB->tcbState) == ThreadState_BlockedOnReply);
+
     word_t next_ptr = call_stack_get_callStackPtr(reply->replyNext);
     word_t prev_ptr = call_stack_get_callStackPtr(reply->replyPrev);
 
@@ -111,17 +111,21 @@ reply_remove(reply_t *reply)
         }
         /* not the head, remove from middle */
         REPLY_PTR(next_ptr)->replyPrev = reply->replyPrev;
-        REPLY_PTR(next_ptr)->replyCaller = reply->replyCaller;
-        if (reply->replyCaller) {
-            /* to maintain the call chain, we remove this caller and
-             * replaced them with the next */
-            reply->replyCaller->tcbReply = REPLY_PTR(next_ptr);
-            reply->replyCaller = NULL;
+        if (REPLY_PTR(next_ptr)->replyTCB) {
+            reply_unlink(REPLY_PTR(next_ptr));
         }
-    } else if (reply->replyCaller) {
+        tcb_t *tcb = reply->replyTCB;
+        assert(tcb);
+        /* to maintain the call chain, we remove this caller and
+         * replaced them with the next - so the TCB that the reply object
+         * we are passing in is linked to is linked to the next reply object, and the
+         * tcb of the next_ptr is dropped */
+        REPLY_PTR(next_ptr)->replyTCB = tcb;
+        reply->replyTCB = NULL;
+        thread_state_ptr_set_replyObject(&tcb->tcbState, REPLY_REF(next_ptr));
+    } else if (reply->replyTCB) {
         /* removing start of call chain */
-        reply->replyCaller->tcbReply = NULL;
-        reply->replyCaller = NULL;
+        reply_unlink(reply);
     }
 
     if (prev_ptr) {
@@ -130,4 +134,45 @@ reply_remove(reply_t *reply)
 
     reply->replyPrev = call_stack_new(0, false);
     reply->replyNext = call_stack_new(0, false);
+}
+
+void reply_remove_tcb(tcb_t *tcb)
+{
+    assert(thread_state_get_tsType(tcb->tcbState) == ThreadState_BlockedOnReply);
+    reply_t *reply = REPLY_PTR(thread_state_get_replyObject(tcb->tcbState));
+    word_t next_ptr = call_stack_get_callStackPtr(reply->replyNext);
+    word_t prev_ptr = call_stack_get_callStackPtr(reply->replyPrev);
+
+
+    if (call_stack_get_isHead(reply->replyNext)) {
+        reply_pop(reply);
+        return;
+    }
+
+    if (next_ptr) {
+        REPLY_PTR(next_ptr)->replyPrev = reply->replyPrev;
+    }
+
+    if (prev_ptr) {
+        REPLY_PTR(prev_ptr)->replyNext = reply->replyNext;
+    }
+
+    reply->replyPrev = call_stack_new(0, false);
+    reply->replyNext = call_stack_new(0, false);
+    reply_unlink(reply);
+}
+
+void reply_clear(reply_t *reply) {
+    assert(reply && reply->replyTCB);
+
+    switch (thread_state_get_tsType(reply->replyTCB->tcbState)) {
+    case ThreadState_BlockedOnReply:
+        reply_remove(reply);
+        break;
+    case ThreadState_BlockedOnReceive:
+        reply_unlink(reply);
+        break;
+    default:
+        fail("Invalid state of replyTCB");
+    }
 }
