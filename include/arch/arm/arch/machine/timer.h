8ae5740ce47e6d3ae50e6c343dae4c124c822825
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-12-05 14:17:56 +1100

    Fix aarch32 low frequency usToTicks
    Prior to this commit we attempted to use reciprocal division to
    divide by 1000, which is required on platforms where calculations must
    be performed in KHz for precision. This overflowed too quickly.
    
    Use div64 specifically for usToTicks, and move getMaxUsToTicks to the
    arch level.

--- include/arch/arm/arch/machine/timer.h
+++ include/arch/arm/arch/machine/timer.h
@@ -17,7 +17,7 @@
 #include <stdint.h>
 #include <types.h>
 #include <util.h>
-
+#include <mode/util.h>
 
 #define USE_KHZ (TIMER_CLOCK_HZ % HZ_IN_MHZ > 0)
 #define TIMER_CLOCK_KHZ (TIMER_CLOCK_HZ / HZ_IN_KHZ)
@@ -34,14 +34,27 @@ static inline CONST time_t ticksToUs(ticks_t ticks);
 #include <plat/machine/timer.h>
 #include <mode/machine/timer.h>
 
+/* get the max value usToTicks can be passed without overflowing */
+static inline CONST time_t
+getMaxUsToTicks(void)
+{
+    if (USE_KHZ) {
+        return UINT64_MAX / TIMER_CLOCK_KHZ;
+    } else {
+        return UINT64_MAX / TIMER_CLOCK_MHZ;
+    }
+}
+
 static inline CONST ticks_t
 usToTicks(time_t us)
 {
     if (USE_KHZ) {
-        return (us * TIMER_CLOCK_KHZ) / KHZ_IN_MHZ;
+        /* reciprocal division overflows too quickly for dividing by KHZ_IN_MHZ.
+         * This operation isn't  used frequently or on many platforms, so use manual
+         * division here */
+        return div64(us * TIMER_CLOCK_KHZ, KHZ_IN_MHZ);
     }
     return us * TIMER_CLOCK_MHZ;
 }
-
 
 #endif /* __ARCH_MACHINE_TIMER_H_ */
