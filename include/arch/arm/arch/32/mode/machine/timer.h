e8a784ebf88497e6af1ad4bf1ce524db0e39b56a
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-12-05 14:17:56 +1100

    Fix aarch32 low frequency usToTicks
    Prior to this commit we attempted to use reciprocal division to
    divide by 1000, which is required on platforms where calculations must
    be performed in KHz for precision. This overflowed too quickly.
    
    Use div64 specifically for usToTicks, and move getMaxUsToTicks to the
    arch level.

--- include/arch/arm/arch/32/mode/machine/timer.h
+++ include/arch/arm/arch/32/mode/machine/timer.h
@@ -17,9 +17,6 @@
 #include <stdint.h>
 #include <util.h>
 
-#define KHZ_IN_HZ_MAGIC 274877907u
-#define KHZ_IN_HZ_SHIFT 38u
-
 /* Use Hypervisor Physical timer */
 #ifdef CONFIG_ARM_HYPERVISOR_SUPPORT
 #define CNT_CT   CNTPCT
@@ -35,17 +32,6 @@
 /* timer function definitions that work for all 32bit arm platforms that provide
  * CLK_MAGIC and TIMER_CLOCK_MHZ -- these definitions might need to move
  * if we come across an arm platform that does not suit this model */
-/* get the max value usToTicks can be passed without overflowing */
-static inline CONST time_t
-getMaxUsToTicks(void)
-{
-    if (USE_KHZ) {
-        return UINT64_MAX / TIMER_CLOCK_KHZ / KHZ_IN_MHZ;
-    } else {
-        return UINT64_MAX / TIMER_CLOCK_MHZ;
-    }
-}
-
 /* get the max value ticksToUs can be passed without overflowing */
 static inline CONST ticks_t
 getMaxTicksToUs(void)
