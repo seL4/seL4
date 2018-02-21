cd4854f4def753d96512243902e84d9b58e12468
Yanyan Shen <yanyan.shen@data61.csiro.au>
2018-02-21 22:34:49 +1100

    armv8: Get CPU index from TPIDR_EL2 when SMP&&HYP.

--- include/arch/arm/arch/64/mode/smp/smp.h
+++ include/arch/arm/arch/64/mode/smp/smp.h
@@ -32,7 +32,11 @@ static inline CONST cpu_id_t
 getCurrentCPUIndex(void)
 {
     cpu_id_t id;
-    asm volatile ("mrs %0, tpidr_el1" : "=r"(id));
+    if (config_set(CONFIG_ARM_HYPERVISOR_SUPPORT)) {
+        asm volatile ("mrs %0, tpidr_el2" : "=r"(id));
+    } else {
+        asm volatile ("mrs %0, tpidr_el1" : "=r"(id));
+    }
     return (id & CPUID_MASK);
 }
 
