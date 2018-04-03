459eb10d663b62c108724e7fd0985cdb71f9ae86
Adrian Danis <Adrian.Danis@data61.csiro.au>
2018-04-03 14:56:08 +1000

    riscv: Remove dead code
    These definitions match no existing kernel interface or abstraction and being removed

--- include/plat/spike/plat/machine/io.h
+++ include/plat/spike/plat/machine/io.h
@@ -28,32 +28,6 @@
 void putDebugChar(unsigned char c);
 unsigned char getDebugChar(void);
 
-#define verbose 5
-#define _dprintf(v, col, args...) \
-            do { \
-                if ((v) < verbose){ \
-                    printf(col); \
-                    plogf(args); \
-                    printf("\033[0;0m"); \
-                } \
-            } while (0)
-
-#define dprintf(v, ...) _dprintf(v, "\033[22;33m", __VA_ARGS__)
-
-#define WARN(...) _dprintf(-1, "\033[1;31mWARNING: ", __VA_ARGS__)
-
-#define NOT_IMPLEMENTED() printf("\033[22;34m %s:%d -> %s not implemented\n\033[;0m",\
-                                  __FILE__, __LINE__, __func__);
-
-void plogf(const char *msg, ...)
-{
-    va_list alist;
-
-    va_start(alist, msg);
-    vprintf(msg, alist);
-    va_end(alist);
-}
-
 #define kernel_putchar(c) putDebugChar(c)
 #else /* !DEBUG */
 #define kernel_putchar(c) ((void)(0))
