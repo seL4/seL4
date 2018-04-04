3f36002ed52f9468bcaee0d0da1c4fad73a0a068
Yanyan Shen <yanyan.shen@data61.csiro.au>
2018-04-05 00:47:44 +1000

    armv8/hyp: Add functions to access registers

--- include/arch/arm/armv/armv8-a/64/armv/vcpu.h
+++ include/arch/arm/armv/armv8-a/64/armv/vcpu.h
@@ -38,6 +38,322 @@
 #define SCTLR_EL1_VM       0x34d58820
 #define SCTLR_DEFAULT      SCTLR_EL1_NATIVE
 
+/* for EL1 SCTLR */
+static inline word_t
+getSCTLR(void)
+{
+    return readSystemControlRegister();
+}
+
+static inline void
+setSCTLR(word_t sctlr)
+{
+    writeSystemControlRegister(sctlr);
+}
+
+static inline word_t
+readTTBR0(void)
+{
+    word_t reg;
+    MRS("ttbr0_el1", reg);
+    return reg;
+}
+
+static inline void
+writeTTBR0(word_t reg)
+{
+    MSR("ttbr0_el1", reg);
+}
+
+static inline word_t
+readTTBR1(void)
+{
+    word_t reg;
+    MRS("ttbr1_el1", reg);
+    return reg;
+}
+
+static inline void
+writeTTBR1(word_t reg)
+{
+    MSR("ttbr1_el1", reg);
+}
+
+static inline word_t
+readTCR(void)
+{
+    word_t reg;
+    MRS("tcr_el1", reg);
+    return reg;
+}
+
+static inline void
+writeTCR(word_t reg)
+{
+    MSR("tcr_el1", reg);
+}
+
+static inline word_t
+readMAIR(void)
+{
+    word_t reg;
+    MRS("mair_el1", reg);
+    return reg;
+}
+
+static inline void
+writeMAIR(word_t reg)
+{
+    MSR("mair_el1", reg);
+}
+
+static inline word_t
+readAMAIR(void)
+{
+    word_t reg;
+    MRS("amair_el1", reg);
+    return reg;
+}
+
+static inline void
+writeAMAIR(word_t reg)
+{
+    MSR("amair_el1", reg);
+}
+
+static inline word_t
+readCIDR(void)
+{
+    uint32_t reg;
+    MRS("contextidr_el1", reg);
+    return (word_t)reg;
+}
+
+static inline void
+writeCIDR(word_t reg)
+{
+    MSR("contextidr_el1", (uint32_t)reg);
+}
+
+static inline word_t
+readACTLR(void)
+{
+    word_t reg;
+    MRS("actlr_el1", reg);
+    return reg;
+}
+
+static inline void
+writeACTLR(word_t reg)
+{
+    MSR("actlr_el1", reg);
+}
+
+static inline word_t
+readAFSR0(void)
+{
+    uint32_t reg;
+    MRS("afsr0_el1", reg);
+    return (word_t)reg;
+}
+
+static inline void
+writeAFSR0(word_t reg)
+{
+    MSR("afsr0_el1", (uint32_t)reg);
+}
+
+static inline word_t
+readAFSR1(void)
+{
+    uint32_t reg;
+    MRS("afsr1_el1", reg);
+    return (word_t)reg;
+}
+
+static inline void
+writeAFSR1(word_t reg)
+{
+    MSR("afsr1_el1", (uint32_t)reg);
+}
+
+static inline word_t
+readESR(void)
+{
+    uint32_t reg;
+    MRS("esr_el1", reg);
+    return (word_t)reg;
+}
+
+static inline void
+writeESR(word_t reg)
+{
+    MSR("esr_el1", (uint32_t)reg);
+}
+
+static inline word_t
+readFAR(void)
+{
+    word_t reg;
+    MRS("far_el1", reg);
+    return reg;
+}
+
+static inline void
+writeFAR(word_t reg)
+{
+    MSR("far_el1", (uint32_t)reg);
+}
+
+/* ISR is read-only */
+static inline word_t
+readISR(void)
+{
+    uint32_t reg;
+    MRS("isr_el1", reg);
+    return (word_t)reg;
+}
+
+static inline word_t
+readVBAR(void)
+{
+    word_t reg;
+    MRS("vbar_el1", reg);
+    return reg;
+}
+
+static inline void
+writeVBAR(word_t reg)
+{
+    MSR("vbar_el1", reg);
+}
+
+static inline word_t
+readTPIDR_EL0(void)
+{
+    word_t reg;
+    MRS("TPIDR_EL0", reg);
+    return reg;
+}
+
+static inline void
+writeTPIDR_EL0(word_t reg)
+{
+    MSR("TPIDR_EL0", reg);
+}
+
+static inline word_t
+readTPIDR_EL1(void)
+{
+    word_t reg;
+    MRS("TPIDR_EL1", reg);
+    return reg;
+}
+
+static inline void
+writeTPIDR_EL1(word_t reg)
+{
+    MSR("TPIDR_EL1", reg);
+}
+
+static inline word_t
+readTPIDRRO_EL0(void)
+{
+    word_t reg;
+    MRS("TPIDRRO_EL0", reg);
+    return reg;
+}
+
+static inline void
+writeTPIDRRO_EL0(word_t reg)
+{
+    MSR("TPIDRRO_EL0", reg);
+}
+
+static inline word_t
+readSP_EL1(void)
+{
+    word_t reg;
+    MRS("sp_el1", reg);
+    return reg;
+}
+
+static inline void
+writeSP_EL1(word_t reg)
+{
+    MSR("sp_el1", reg);
+}
+
+static inline word_t
+readELR_EL1(void)
+{
+    word_t reg;
+    MRS("elr_el1", reg);
+    return reg;
+}
+
+static inline void
+writeELR_EL1(word_t reg)
+{
+    MRS("elr_el1", reg);
+}
+
+static inline word_t
+readSPSR_EL1(void)
+{
+    word_t reg;
+    MRS("spsr_el1", reg);
+    return reg;
+}
+
+static inline void
+writeSPSR_EL1(word_t reg)
+{
+    MSR("spsr_el1", reg);
+}
+
+static inline word_t
+readCPACR_EL1(void)
+{
+    word_t reg;
+    MRS("cpacr_el1", reg);
+    return reg;
+}
+
+static inline void
+writeCPACR_EL1(word_t reg)
+{
+    MSR("cpacr_el1", reg);
+}
+
+static inline word_t
+readCNTV_TVAL_EL0(void)
+{
+    word_t reg;
+    MRS("cntv_tval_el0", reg);
+    return reg;
+}
+
+static inline void
+writeCNTV_TVAL_EL0(word_t reg)
+{
+    MSR("cntv_tval_el0", reg);
+}
+
+static inline word_t
+readCNTV_CTL_EL0(void)
+{
+    word_t reg;
+    MRS("cntv_ctl_el0", reg);
+    return reg;
+}
+
+static inline void
+writeCNTV_CTL_EL0(word_t reg)
+{
+    MSR("cntv_ctl_el0", reg);
+}
+
 #endif /* End of CONFIG_ARM_HYPERVISOR_SUPPORT */
 
 #endif
