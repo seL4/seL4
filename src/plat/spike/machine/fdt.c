50365f91a26ef9d64ed37c0c151e83b491a9b37d
Adrian Danis <Adrian.Danis@data61.csiro.au>
2018-04-09 16:01:16 +1000

    riscv: Remove completed todo

--- src/plat/spike/machine/fdt.c
+++ src/plat/spike/machine/fdt.c
@@ -76,8 +76,6 @@ const char fdt_reg[] = {'r', 'e', 'g', 0};
 const char fdt_device_type[] = {'d', 'e', 'v', 'i', 'c', 'e', '_', 't', 'y', 'p', 'e', 0};
 const char fdt_memory[] = {'m', 'e', 'm', 'o', 'r', 'y', 0};
 
-// RVTODO: this code needs to updated to not have function pointers or string literals "like this"
-// and any of these appropriate string functions should be moved to common utils
 static inline uint32_t bswap(uint32_t x)
 {
     uint32_t y = (x & 0x00FF00FF) <<  8 | (x & 0xFF00FF00) >>  8;
