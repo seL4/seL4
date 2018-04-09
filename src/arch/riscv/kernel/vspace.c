d205615c53e6e80019d5de98c98ea562ee8c5084
Adrian Danis <Adrian.Danis@data61.csiro.au>
2018-04-09 16:54:37 +1000

    riscv: Be consistent with PT mapping faults
    Other architectures claim `seL4_DeleteFirst` as the error if the only way to map a page table
    is to first delete something. This unifies the riscv code to also state `seL4_DeleteFirst`
    when there is already a lowest level page table mapped instead of lookup fault.

--- src/arch/riscv/kernel/vspace.c
+++ src/arch/riscv/kernel/vspace.c
@@ -740,18 +740,11 @@ decodeRISCVPageTableInvocation(word_t label, unsigned int length,
 
     lookupPTSlot_ret_t lu_ret = lookupPTSlot(lvl1pt, vaddr);
 
-    /* is the slot valid for a pte ? */
-    if (lu_ret.ptBitsLeft == seL4_PageBits) {
-        userError("RISCVPageTableMap: all page tables mapped at this address");
-        current_lookup_fault = lookup_fault_missing_capability_new(lu_ret.ptBitsLeft);
-        current_syscall_error.type = seL4_FailedLookup;
-        current_syscall_error.failedLookupWasSource = false;
-        return EXCEPTION_SYSCALL_ERROR;
-    }
-
-    /* is the slot empty ? */
-    if (pte_ptr_get_valid(lu_ret.ptSlot)) {
-        userError("RISCVPageTableMap: frame already mapped at this address");
+    /* if there is already something mapped (valid is set) or we have traversed far enough
+     * that a page table is not valid to map then tell the user that they ahve to delete
+     * something before they can put a PT here */
+    if (lu_ret.ptBitsLeft == seL4_PageBits || pte_ptr_get_valid(lu_ret.ptSlot)) {
+        userError("RISCVPageTableMap: All objects mapped at this address");
         current_syscall_error.type = seL4_DeleteFirst;
         return EXCEPTION_SYSCALL_ERROR;
     }
