c2212688ceabfacc879be35907d22806e9b4e90f
Adrian Danis <Adrian.Danis@data61.csiro.au>
2018-05-08 16:41:28 +1000

    x86: IOPort invocation has proper structure
    Invocations should have a seperate decode and invoke stage and the previous invocation
    performed stateful operations during the notional 'decode' phase, and did not use a
    separate function for the invoke stage. This provides the correct structure by splitting
    the decode into two halves, one for the 'in' operations, and one for the 'out' operations,
    and defines an invoke for each of them.
    
    This solution has some duplication due to repeatedly decoding the invocation label to
    determine the size of the port operation, but should ultimately be the simplist solution
    to verify.

--- src/arch/x86/object/ioport.c
+++ src/arch/x86/object/ioport.c
@@ -178,6 +178,68 @@ decodeX86PortControlInvocation(
     return invokeX86PortControl(first_port, last_port, destSlot, slot);
 }
 
+static exception_t
+invokeX86PortIn(word_t invLabel, uint16_t port)
+{
+    uint32_t res;
+    word_t len;
+
+    switch (invLabel) {
+    case X86IOPortIn8:
+        res = in8(port);
+        break;
+    case X86IOPortIn16:
+        res = in16(port);
+        break;
+    case X86IOPortIn32:
+        res = in32(port);
+        break;
+    }
+
+    setRegister(NODE_STATE(ksCurThread), badgeRegister, 0);
+
+    if (n_msgRegisters < 1) {
+        word_t* ipcBuffer;
+        ipcBuffer = lookupIPCBuffer(true, NODE_STATE(ksCurThread));
+        if (ipcBuffer != NULL) {
+            ipcBuffer[1] = res;
+            len = 1;
+        } else {
+            len = 0;
+        }
+    } else {
+        setRegister(NODE_STATE(ksCurThread), msgRegisters[0], res);
+        len = 1;
+    }
+
+    setRegister(NODE_STATE(ksCurThread), msgInfoRegister,
+                wordFromMessageInfo(seL4_MessageInfo_new(0, 0, 0, len)));
+
+    return EXCEPTION_NONE;
+}
+
+static exception_t
+invokeX86PortOut(word_t invLabel, uint16_t port, uint32_t data)
+{
+    switch (invLabel) {
+    case X86IOPortIn8:
+        out8(port, data);
+        break;
+    case X86IOPortIn16:
+        out16(port, data);
+        break;
+    case X86IOPortIn32:
+        out32(port, data);
+        break;
+    }
+
+    setRegister(NODE_STATE(ksCurThread), badgeRegister, 0);
+    setRegister(NODE_STATE(ksCurThread), msgInfoRegister,
+                wordFromMessageInfo(seL4_MessageInfo_new(0, 0, 0, 0)));
+
+    return EXCEPTION_NONE;
+}
+
 exception_t
 decodeX86PortInvocation(
     word_t invLabel,
@@ -189,149 +251,64 @@ decodeX86PortInvocation(
     word_t* buffer
 )
 {
-    uint32_t res;
-    uint32_t len;
-    uint16_t port;
     exception_t ret;
 
-    /* Ensure user specified at very least a port. */
-    if (length < 1) {
-        userError("IOPort: Truncated message.");
-        current_syscall_error.type = seL4_TruncatedMessage;
-        return EXCEPTION_SYSCALL_ERROR;
-    }
-
-    if (invLabel == X86IOPortOut8 || invLabel == X86IOPortOut16 || invLabel == X86IOPortOut32) {
-        /* Ensure the incoming message is long enough for the write. */
-        if (length < 2) {
-            userError("IOPort Out32: Truncated message.");
+    if (invLabel == X86IOPortIn8 || invLabel == X86IOPortIn16 || invLabel == X86IOPortIn32) {
+        if (length < 1) {
+            userError("IOPort: Truncated message.");
             current_syscall_error.type = seL4_TruncatedMessage;
             return EXCEPTION_SYSCALL_ERROR;
         }
-    }
-
-    /* Get the port the user is trying to write to. */
-    port = getSyscallArg(0, buffer) & 0xffff;
-
-    switch (invLabel) {
-    case X86IOPortIn8: { /* inport 8 bits */
-
-        /* Check we are allowed to perform the operation. */
-        ret = ensurePortOperationAllowed(cap, port, 1);
+        /* Get the port the user is trying to read from. */
+        uint16_t port = getSyscallArg(0, buffer) & 0xffff;
+        switch (invLabel) {
+        case X86IOPortIn8:
+            ret = ensurePortOperationAllowed(cap, port, 1);
+            break;
+        case X86IOPortIn16:
+            ret = ensurePortOperationAllowed(cap, port, 2);
+            break;
+        case X86IOPortIn32:
+            ret = ensurePortOperationAllowed(cap, port, 4);
+            break;
+        }
         if (ret != EXCEPTION_NONE) {
             return ret;
         }
+        setThreadState(NODE_STATE(ksCurThread), ThreadState_Restart);
+        return invokeX86PortIn(invLabel, port);
+    } else if (invLabel == X86IOPortOut8 || invLabel == X86IOPortOut16 || invLabel == X86IOPortOut32) {
+        /* Ensure the incoming message is long enough for the write. */
+        if (length < 2) {
+            userError("IOPort Out: Truncated message.");
+            current_syscall_error.type = seL4_TruncatedMessage;
+            return EXCEPTION_SYSCALL_ERROR;
+        }
+        /* Get the port the user is trying to write to. */
+        uint16_t port = getSyscallArg(0, buffer) & 0xffff;
+        uint32_t data = getSyscallArg(1, buffer) & 0xffffffff;
 
-        /* Perform the read. */
-        res = in8(port);
-        len = 1;
-        break;
-    }
-
-    case X86IOPortIn16: { /* inport 16 bits */
-
-        /* Check we are allowed to perform the operation. */
-        ret = ensurePortOperationAllowed(cap, port, 2);
+        switch (invLabel) {
+        case X86IOPortOut8:
+            ret = ensurePortOperationAllowed(cap, port, 1);
+            break;
+        case X86IOPortOut16:
+            ret = ensurePortOperationAllowed(cap, port, 2);
+            break;
+        case X86IOPortOut32:
+            ret = ensurePortOperationAllowed(cap, port, 4);
+            break;
+        }
         if (ret != EXCEPTION_NONE) {
             return ret;
         }
-
-        /* Perform the read. */
-        res = in16(port);
-        len = 1;
-        break;
-    }
-
-    case X86IOPortIn32: { /* inport 32 bits */
-
-        /* Check we are allowed to perform the operation. */
-        ret = ensurePortOperationAllowed(cap, port, 4);
-        if (ret != EXCEPTION_NONE) {
-            return ret;
-        }
-
-        /* Perform the read. */
-        res = in32(port);
-        len = 1;
-        break;
-    }
-
-    case X86IOPortOut8: { /* outport 8 bits */
-        uint8_t data;
-
-        /* Check we are allowed to perform the operation. */
-        ret = ensurePortOperationAllowed(cap, port, 1);
-        if (ret != EXCEPTION_NONE) {
-            return ret;
-        }
-
-        /* Perform the write. */
-        data = (getSyscallArg(1, buffer)) & 0xff;
-        out8(port, data);
-        len = 0;
-        break;
-    }
-
-    case X86IOPortOut16: { /* outport 16 bits */
-        uint16_t data;
-
-        /* Check we are allowed to perform the operation. */
-        ret = ensurePortOperationAllowed(cap, port, 2);
-        if (ret != EXCEPTION_NONE) {
-            return ret;
-        }
-
-        /* Perform the write. */
-        data = (getSyscallArg(1, buffer)) & 0xffff;
-        out16(port, data);
-        len = 0;
-        break;
-    }
-
-    case X86IOPortOut32: { /* outport 32 bits */
-        uint32_t data;
-
-        /* Check we are allowed to perform the operation. */
-        ret = ensurePortOperationAllowed(cap, port, 4);
-        if (ret != EXCEPTION_NONE) {
-            return ret;
-        }
-
-        /* Perform the write. */
-        data = getSyscallArg(1, buffer) & 0xffffffff;
-        out32(port, data);
-        len = 0;
-        break;
-    }
-
-    default:
+        setThreadState(NODE_STATE(ksCurThread), ThreadState_Restart);
+        return invokeX86PortOut(invLabel, port, data);
+    } else {
         userError("IOPort: Unknown operation.");
         current_syscall_error.type = seL4_IllegalOperation;
         return EXCEPTION_SYSCALL_ERROR;
     }
-
-    if (len > 0) {
-        /* return the value read from the port */
-        setRegister(NODE_STATE(ksCurThread), badgeRegister, 0);
-        if (n_msgRegisters < 1) {
-            word_t* ipcBuffer;
-            ipcBuffer = lookupIPCBuffer(true, NODE_STATE(ksCurThread));
-            if (ipcBuffer != NULL) {
-                ipcBuffer[1] = res;
-                len = 1;
-            } else {
-                len = 0;
-            }
-        } else {
-            setRegister(NODE_STATE(ksCurThread), msgRegisters[0], res);
-            len = 1;
-        }
-    }
-    setRegister(NODE_STATE(ksCurThread), msgInfoRegister,
-                wordFromMessageInfo(seL4_MessageInfo_new(0, 0, 0, len)));
-
-    setThreadState(NODE_STATE(ksCurThread), ThreadState_Restart);
-    return EXCEPTION_NONE;
 }
 
 void
