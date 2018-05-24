f6d4d98777fcb9b420147742b371b72e583a8eb4
Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
2018-05-24 12:43:28 +0100

    spike: Use mode-aware definitions when dealing with interrupts

--- src/plat/spike/machine/hardware.c
+++ src/plat/spike/machine/hardware.c
@@ -11,6 +11,16 @@
  */
 
 /*
+ * Copyright (c) 2018, Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
+ * All rights reserved.
+ *
+ * This software was was developed in part by SRI International and the University of
+ * Cambridge Computer Laboratory (Department of Computer Science and
+ * Technology) under DARPA contract HR0011-18-C-0016 ("ECATS"), as part of the
+ * DARPA SSITH research programme.
+ */
+
+/*
  *
  * Copyright 2016, 2017 Hesham Almatary, Data61/CSIRO <hesham.almatary@data61.csiro.au>
  * Copyright 2015, 2016 Hesham Almatary <heshamelmatary@gmail.com>
@@ -26,6 +36,7 @@
 #include <plat/machine/devices.h>
 #include <plat/machine/hardware.h>
 #include <arch/sbi.h>
+#include <arch/encoding.h>
 
 #define MAX_AVAIL_P_REGS 2
 
@@ -62,8 +73,7 @@ interrupt_t
 getActiveIRQ(void)
 {
 
-    uint64_t temp = 0;
-    asm volatile ("csrr %0, scause":"=r" (temp)::);
+    word_t temp = read_csr_env(cause);
 
     if (!(temp & BIT(CONFIG_WORD_SIZE - 1))) {
         return irqInvalid;
@@ -90,9 +100,9 @@ maskInterrupt(bool_t disable, interrupt_t irq)
             clear_csr(sie, BIT(irq));
         }
     } else {
-        /* FIXME: currently only account for user/supervisor and timer interrupts */
-        if (irq == 4 /* user timer */ || irq == 5 /* supervisor timer */) {
-            set_csr(sie, BIT(irq));
+        /* FIXME: currently only account for timer interrupts */
+        if (irq == KERNEL_TIMER_IRQ) {
+            set_csr_env(ie, BIT(irq));
         } else {
             /* TODO: account for external and PLIC interrupts */
         }
@@ -120,7 +130,7 @@ ackInterrupt(irq_t irq)
     // don't ack the kernel timer interrupt, see the comment in resetTimer
     // to understand why
     if (irq != KERNEL_TIMER_IRQ) {
-        clear_csr(sip, BIT(irq));
+        clear_csr_env(ip, BIT(irq));
     }
     //set_csr(scause, 0);
 
@@ -162,7 +172,7 @@ resetTimer(void)
     // ack the timer interrupt. we do this here as due to slow simulation platform there
     // is a race between us setting the new interrupt here, and the ackInterrupt call in
     // handleInterrupt that will happen at some point after this call to resetTimer
-    clear_csr(sip, KERNEL_TIMER_IRQ);
+    clear_csr_env(ie, KERNEL_TIMER_IRQ);
     // repeatedly try and set the timer in a loop as otherwise there is a race and we
     // may set a timeout in the past, resulting in it never getting triggered
     do {
