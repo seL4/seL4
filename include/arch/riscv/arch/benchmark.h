e1e6b9009a2e54698041f3c38bac3b7184c165c2
Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
2018-05-28 14:21:14 +0100

    riscv/benchmarks: Implement timestamp function

--- include/arch/riscv/arch/benchmark.h
+++ include/arch/riscv/arch/benchmark.h
@@ -11,6 +11,16 @@
  */
 
 /*
+ * Copyright (c) 2018, Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
+ * All rights reserved.
+ *
+ * This software was was developed in part by SRI International and the University of
+ * Cambridge Computer Laboratory (Department of Computer Science and
+ * Technology) under DARPA contract HR0011-18-C-0016 ("ECATS"), as part of the
+ * DARPA SSITH research programme.
+ */
+
+/*
  *
  * Copyright 2016, 2017 Hesham Almatary, Data61/CSIRO <hesham.almatary@data61.csiro.au>
  * Copyright 2015, 2016 Hesham Almatary <heshamelmatary@gmail.com>
@@ -20,9 +30,20 @@
 #define ARCH_BENCHMARK_H
 
 #include <config.h>
-#include <arch/object/structures.h>
+#include <arch/encoding.h>
 
-#ifdef CONFIG_ENABLE_BENCHMARK
-#error "RISC-V doesn't support timestamp() function yet"
-#endif /* CONFIG_ENABLE_BENCHMARK */
+#ifdef CONFIG_ENABLE_BENCHMARKS
+static inline timestamp_t
+timestamp(void)
+{
+    timestamp_t time = read_csr(cycle);
+
+    if (config_set(CONFIG_ARCH_RISCV_RV32)) {
+        time |= (timestamp_t) (read_csr(cycleh) << 32);
+    }
+
+    return time;
+}
+
+#endif /* CONFIG_ENABLE_BENCHMARKS */
 #endif /* ARCH_BENCHMARK_H */
