028b520a06a7cb29bb8e4b8f7343a809412b4cc1
Chris Guikema <chris.guikema@dornerworks.com>
2018-07-25 10:43:21 -0400

    riscv: Order read/writes before writing sptbr
    This instruction is required when more than one thread exists with
    different ASIDs. The system will lock up after the first context
    switch when running on hardware.
    
    Issue first noticed and fixed by Hesham Almatary
    <Hesham.Almatary@cl.cam.ac.uk>
    
    Change-Id: I6eb64df6b584ff7de79c8af30b28bbc7bb234643

--- include/arch/riscv/arch/machine.h
+++ include/arch/riscv/arch/machine.h
@@ -117,6 +117,9 @@ static inline void setVSpaceRoot(paddr_t addr, asid_t asid)
                            asid,                         /* asid */
                            addr >> seL4_PageBits); /* PPN */
 
+    /* Order read/write operations */
+    sfence();
+
     /* Current toolchain still uses sptbr register name although it got renamed in priv-1.10.
      * This will most likely need to change with newer toolchains
      */
