a3fb0fb0b33b31391d82b0c2382ec9cd375490c6
Adrian Danis <Adrian.Danis@data61.csiro.au>
2018-08-08 13:40:40 +1000

    riscv: Simplify map_kernel_window
    Removes an unneeded loop in map_kernel_window and then simplifies the contained code.

--- src/arch/riscv/kernel/vspace.c
+++ src/arch/riscv/kernel/vspace.c
@@ -123,46 +123,32 @@ map_kernel_window(void)
     assert(pptr == KERNEL_BASE);
     paddr = PADDR_LOAD;
 
+    /* Sv39: if both phy and virt address are aligned on 1GiB boundary, use Level 1 mappings
+     * Sv32: this checks if phy and virt address are aligned on 2MiB boundary.
+     */
+    if (IS_ALIGNED(pptr, RISCV_GET_LVL_PGSIZE_BITS(1)) &&
+            IS_ALIGNED(paddr, RISCV_GET_LVL_PGSIZE_BITS(1))) {
+        kernel_root_pageTable[RISCV_GET_PT_INDEX(pptr, 1)] = pte_next(paddr, true);
+        pptr += RISCV_GET_LVL_PGSIZE(1);
+        paddr += RISCV_GET_LVL_PGSIZE(1);
+    }
 #if CONFIG_PT_LEVELS == 3
-    word_t orig_indx_1GiB = RISCV_GET_PT_INDEX(pptr, 1);
-#endif
-
-    while (pptr < UINTPTR_MAX - RISCV_GET_LVL_PGSIZE(1) + 1) {
-
-        /* Sv39: if both phy and virt address are aligned on 1GiB boundary, use Level 1 mappings
-         * Sv32: this checks if phy and virt address are aligned on 2MiB boundary.
-         */
-        if (IS_ALIGNED(pptr, RISCV_GET_LVL_PGSIZE_BITS(1)) &&
-                IS_ALIGNED(paddr, RISCV_GET_LVL_PGSIZE_BITS(1))) {
-            kernel_root_pageTable[RISCV_GET_PT_INDEX(pptr, 1)] = pte_next(paddr, true);
-            pptr += RISCV_GET_LVL_PGSIZE(1);
-            paddr += RISCV_GET_LVL_PGSIZE(1);
-        }
-#if CONFIG_PT_LEVELS == 3
-        else if (IS_ALIGNED(pptr, RISCV_GET_LVL_PGSIZE_BITS(2)) &&
-                 IS_ALIGNED(paddr, RISCV_GET_LVL_PGSIZE_BITS(2))) {
-            word_t indx_1GiB = RISCV_GET_PT_INDEX(pptr, 1);
-            word_t indx_2MiB = RISCV_GET_PT_INDEX(pptr, 2);
-            word_t level2_ref = indx_1GiB % orig_indx_1GiB;
-
-            assert(level2_ref < NUM_2MB_ENTRIES);
-
-            if (!isPTEPageTable(&kernel_root_pageTable[indx_1GiB])) {
-                // Check if it is already pointing to a 2MiB page table
-                // if not, make a next level page table
-                // otherwise continue filling in the second level page table
-                kernel_root_pageTable[indx_1GiB] =
-                    pte_next(kpptr_to_paddr(kernel_level2_Tables[level2_ref]), false);
-            }
-
-            kernel_level2_Tables[level2_ref][indx_2MiB] = pte_next(paddr, true);
+    else if (IS_ALIGNED(pptr, RISCV_GET_LVL_PGSIZE_BITS(2)) &&
+             IS_ALIGNED(paddr, RISCV_GET_LVL_PGSIZE_BITS(2))) {
+        word_t indx_1GiB = RISCV_GET_PT_INDEX(pptr, 1);
+        word_t indx_2MiB = 0;
+        kernel_root_pageTable[indx_1GiB] =
+            pte_next(kpptr_to_paddr(kernel_level2_Tables[0]), false);
+        while (pptr < KERNEL_BASE + RISCV_GET_LVL_PGSIZE(1)) {
+            kernel_level2_Tables[0][indx_2MiB] = pte_next(paddr, true);
+            indx_2MiB++;
             pptr += RISCV_GET_LVL_PGSIZE(2);
             paddr += RISCV_GET_LVL_PGSIZE(2);
         }
+    }
 #endif
-        else {
-            fail("Kernel not properly aligned");
-        }
+    else {
+        fail("Kernel not properly aligned");
     }
 
     /* There should be 1GiB free where we will put device mapping some day */
