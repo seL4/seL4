9d0db528b305f9b046b1ab6cadac9cd1c2ccf5c1
Edward Pierzchalski <ed.pierzchalski@data61.csiro.au>
2018-12-05 10:48:40 +1100

    riscv: remove invalid 'void CONST'.
    Normally a warning, but promoted by [-Werror=attributes].

--- include/arch/riscv/arch/kernel/thread.h
+++ include/arch/riscv/arch/kernel/thread.h
@@ -24,7 +24,7 @@
 void Arch_switchToThread(tcb_t *tcb);
 void Arch_switchToIdleThread(void);
 void Arch_configureIdleThread(tcb_t *tcb);
-void CONST Arch_activateIdleThread(tcb_t *tcb);
+void Arch_activateIdleThread(tcb_t *tcb);
 
 static inline bool_t CONST
 Arch_getSanitiseRegisterInfo(tcb_t *thread)
