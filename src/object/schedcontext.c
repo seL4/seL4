9ccdc857e77ec0e654e66646375e1a38e368aaaa
Anna Lyons <Anna.Lyons@data61.csiro.au>
2019-02-13 11:31:37 +1100

    mcs: Use rescheduleRequired in schedcontext_bind
    Schedcontext_bind is currently called in ThreadControl, which
    manipulates capabilities which in turn can result in KsSchedulerAction
    being deleted. This means that we cannot use possibleSwitchTo in this
    function.
    
    This is a stop-gap fix for verification, the long term fix is to split
    setting scheduling parameters from ThreadControl, then we can return to
    a direct switch, and also do a direct switch for other thread/scheduler
    settings like priorities.

--- src/object/schedcontext.c
+++ src/object/schedcontext.c
@@ -273,7 +273,13 @@ void schedContext_bindTCB(sched_context_t *sc, tcb_t *tcb)
 
     schedContext_resume(sc);
     if (isSchedulable(tcb)) {
-        possibleSwitchTo(tcb);
+        SCHED_ENQUEUE(tcb);
+        rescheduleRequired();
+        // TODO -- at some stage we should take this call out of any TCB invocations that
+        // alter capabilities, so that we can do a direct switch. The prefernce here is to
+        // remove seL4_SetSchedParams from using ThreadControl. It's currently out of scope for
+        // verification work, so the work around is to use rescheduleRequired()
+        //possibleSwitchTo(tcb);
     }
 }
 
