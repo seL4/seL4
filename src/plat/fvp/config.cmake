7900b6dccd3cf5f78379448f74637f0d72e89948
Yanyan Shen <yanyan.shen@data61.csiro.au>
2019-02-18 10:20:51 +1100

    aarch64: Add initial Arm FVP platform config
    This platform assumes 2 clusters of A57 processors as described in
    tools/dts/fvp.dts. This configuration is for running on FVP simulators.

---
+++ src/plat/fvp/config.cmake
@@ -0,0 +1,35 @@
+#
+# Copyright 2019, Data61
+# Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+# ABN 41 687 119 230.
+#
+# This software may be distributed and modified according to the terms of
+# the GNU General Public License version 2. Note that NO WARRANTY is provided.
+# See "LICENSE_GPLv2.txt" for details.
+#
+# @TAG(DATA61_GPL)
+#
+
+cmake_minimum_required(VERSION 3.7.2)
+
+declare_platform(fvp KernelPlatformFVP-evk PLAT_FVP KernelSel4ArchAarch64)
+
+if(KernelPlatformFVP)
+    declare_seL4_arch(aarch64)
+    set(KernelArmCortexA57 ON)
+    set(KernelArchArmV8a ON)
+    config_set(KernelPlatform PLAT "fvp")
+    set(KernelArmPASizeBits44 ON)
+    list(APPEND KernelDTSList "tools/dts/fvp.dts")
+    declare_default_headers(
+        TIMER_FREQUENCY 100000000llu
+        MAX_IRQ 207
+        INTERRUPT_CONTROLLER arch/machine/gic_v3.h
+        TIMER drivers/timer/arm_priv.h
+    )
+endif()
+
+add_sources(
+    DEP "KernelPlatformFVP"
+    CFILES src/arch/arm/machine/l2c_nop.c src/arch/arm/machine/gic_v3.c
+)
