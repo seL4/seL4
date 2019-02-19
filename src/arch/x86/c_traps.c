a3253623940491275ae8d22e6ddcd7462d25c0e7
Anna Lyons <Anna.Lyons@data61.csiro.au>
2019-02-19 16:10:13 +1100

    x86: check vcpu is set on seL4_VMEnter
    Prio to this change the kernel would crash if a thread with no VCPU
    attempted to seL4_VMEnter. As of this commit, an unknown syscall exception
    will be raised and the kernel will not crash.

--- src/arch/x86/c_traps.c
+++ src/arch/x86/c_traps.c
@@ -111,7 +111,7 @@ slowpath(syscall_t syscall)
 {
 
 #ifdef CONFIG_VTX
-    if (syscall == SysVMEnter) {
+    if (syscall == SysVMEnter && NODE_STATE(ksCurThread)->tcbArch.tcbVCPU) {
         vcpu_update_state_sysvmenter(NODE_STATE(ksCurThread)->tcbArch.tcbVCPU);
         if (NODE_STATE(ksCurThread)->tcbBoundNotification && notification_ptr_get_state(NODE_STATE(ksCurThread)->tcbBoundNotification) == NtfnState_Active) {
             completeSignal(NODE_STATE(ksCurThread)->tcbBoundNotification, NODE_STATE(ksCurThread));
