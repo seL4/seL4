9e493a671eae29c852fd33bddd02a4fa03cfc450
Curtis Millar <curtis.millar@data61.csiro.au>
2019-07-24 16:09:05 +1000

    mcs: Don't pass capacity through calls
    The capacity does not need to be passed as an argument to
    refill_check_budget as all information that it was being used for can be
    dertermined from the usage directly.
    
    The additional case handling for overrun into a partial refill is also
    handled by split check, so that is used in all cases where any partial
    usage remains.
    
    Signed-off-by: Curtis Millar <curtis.millar@data61.csiro.au>

--- include/kernel/thread.h
+++ include/kernel/thread.h
@@ -217,7 +217,7 @@ static inline void updateRestartPC(tcb_t *tcb)
 void endTimeslice(bool_t can_timeout_fault);
 
 /* called when a thread has used up its head refill */
-void chargeBudget(ticks_t capacity, ticks_t consumed, bool_t canTimeoutFault, word_t core, bool_t isCurCPU);
+void chargeBudget(ticks_t consumed, bool_t canTimeoutFault, word_t core, bool_t isCurCPU);
 
 /* Update the kernels timestamp and stores in ksCurTime.
  * The difference between the previous kernel timestamp and the one just read
@@ -260,7 +260,7 @@ static inline bool_t checkBudget(void)
         return true;
     }
 
-    chargeBudget(capacity, NODE_STATE(ksConsumed), true, CURRENT_CPU_INDEX(), true);
+    chargeBudget(NODE_STATE(ksConsumed), true, CURRENT_CPU_INDEX(), true);
     return false;
 }
 
