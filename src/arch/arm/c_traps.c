77b838dd4c0c267b0bd316b2e429c5d50a7ee1cf
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2019-07-29 11:06:17 +1000

    arm, smp: Correctly transform irq index into ID
    Under SMP configurations, getActiveIRQ doesn't directly return an IRQ
    ID, but an encoding of the ID and core due to the existance of per core
    interrupts. This fixes some remaining locations where the result is
    still interpreted as an IRQ.

--- src/arch/arm/c_traps.c
+++ src/arch/arm/c_traps.c
@@ -95,7 +95,7 @@ void VISIBLE NORETURN c_handle_instruction_fault(void)
 
 void VISIBLE NORETURN c_handle_interrupt(void)
 {
-    NODE_LOCK_IRQ_IF(getActiveIRQ() != irq_remote_call_ipi);
+    NODE_LOCK_IRQ_IF(IDX_TO_IRQ(getActiveIRQ()) != irq_remote_call_ipi);
     c_entry_hook();
 
 #ifdef TRACK_KERNEL_ENTRIES
