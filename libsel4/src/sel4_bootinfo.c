c8b81c28ba988401053b0536a18dd8147f26d375
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2019-08-01 16:34:40 +1000

    libsel4: Don't use userData for storing IPC buffer
    userData is no longer needed to hold a reference to the IPC buffer. The
    IPC buffer is now available as a thread local variable.

--- libsel4/src/sel4_bootinfo.c
+++ libsel4/src/sel4_bootinfo.c
@@ -22,8 +22,6 @@ seL4_BootInfo *bootinfo;
 void seL4_InitBootInfo(seL4_BootInfo *bi)
 {
     bootinfo = bi;
-    /* Save the address of the IPC buffer for seL4_GetIPCBuffer on IA32. */
-    seL4_SetUserData((seL4_Word)bootinfo->ipcBuffer);
 }
 
 seL4_BootInfo *seL4_GetBootInfo()
