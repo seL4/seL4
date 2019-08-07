d9f809015bcb9684363440fee7613d0c2ae74bf6
Yanyan Shen <yanyan.shen@data61.csiro.au>
2019-08-07 10:50:18 +1000

    trivial: Fix style

--- include/arch/riscv/arch/smp/ipi.h
+++ include/arch/riscv/arch/smp/ipi.h
@@ -25,6 +25,16 @@ typedef enum {
 void ipi_send_target(irq_t irq, word_t cpuTargetList);
 irq_t ipi_get_irq(void);
 void  ipi_clear_irq(irq_t irq);
+#else
+static inline irq_t ipi_get_irq(void)
+{
+    return irqInvalid;
+}
+
+static inline void  ipi_clear_irq(irq_t irq)
+{
+    return;
+}
 #endif
 
 #endif /* __ARCH_SMP_IPI_H */
