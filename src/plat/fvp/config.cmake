929dead3c370a05a9b799327b8db659e687a79a4
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2019-08-12 15:31:13 +1000

    CMake,fvp: Fix typos in FVP CMake config
    - Use correct timer header file
    - Use correct name for FVP target.

--- src/plat/fvp/config.cmake
+++ src/plat/fvp/config.cmake
@@ -12,19 +12,19 @@
 
 cmake_minimum_required(VERSION 3.7.2)
 
-declare_platform(fvp KernelPlatformFVP-evk PLAT_FVP KernelSel4ArchAarch64)
+declare_platform(fvp KernelPlatformFVP PLAT_FVP KernelSel4ArchAarch64)
 
 if(KernelPlatformFVP)
     declare_seL4_arch(aarch64)
     set(KernelArmCortexA57 ON)
     set(KernelArchArmV8a ON)
-    config_set(KernelPlatform PLAT "fvp")
+    config_set(KernelARMPlatform PLAT "fvp")
     list(APPEND KernelDTSList "tools/dts/fvp.dts")
     declare_default_headers(
         TIMER_FREQUENCY 100000000llu
         MAX_IRQ 207
         INTERRUPT_CONTROLLER arch/machine/gic_v3.h
-        TIMER drivers/timer/arm_priv.h
+        TIMER drivers/timer/arm_generic.h
     )
 endif()
 
