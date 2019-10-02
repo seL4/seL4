77b3521710963af45f38bd6c60e4a712c5c487e9
Qian Ge <qian.ge@data61.csiro.au>
2019-10-02 12:07:55 +1000

    smmu header file in object

---
+++ include/arch/arm/arch/object/smmu.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2019, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(DATA61_GPL)
+ */
+
+
+#ifndef __ARCH_OBJECT_SMMU_H
+#define __ARCH_OBJECT_SMMU_H
+
+
+#define SMMU_SID_CNODE_SLOT_BITS    8 
+
+#define SMMU_CB_CNODE_SLOT_BITS     6
+
+#endif  /*__ARCH_OBJECT_SMMU_H*/
