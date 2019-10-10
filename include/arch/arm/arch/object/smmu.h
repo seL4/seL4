0cf122a0409a5da6f866bed3fa775ed6b5d34532
Qian Ge <qian.ge@data61.csiro.au>
2019-10-10 15:39:36 +1100

    SMMU: system calls for creating SID and CB caps
    Providing system calls on stream ID control cap and context bank
    control cap for creating stream ID and context bank caps.
    
    Signed-off-by: Oliver Scott <Oliver.Scott@data61.csiro.au>

--- include/arch/arm/arch/object/smmu.h
+++ include/arch/arm/arch/object/smmu.h
@@ -4,11 +4,24 @@
  * SPDX-License-Identifier: GPL-2.0-only
  */
 
-#ifndef __ARCH_OBJECT_SMMU_H
-#define __ARCH_OBJECT_SMMU_H
+#pragma once
 
 
 #define SMMU_SID_CNODE_SLOT_BITS    8
 #define SMMU_CB_CNODE_SLOT_BITS     6
 
-#endif  /*__ARCH_OBJECT_SMMU_H*/
+exception_t decodeARMSIDControlInvocation(word_t label, unsigned int length, cptr_t cptr,
+                                          cte_t *srcSlot, cap_t cap, extra_caps_t extraCaps,
+                                          bool_t call, word_t *buffer);
+
+exception_t decodeARMSIDInvocation(word_t label, unsigned int length, cptr_t cptr,
+                                   cte_t *srcSlot, cap_t cap, extra_caps_t extraCaps,
+                                   bool_t call, word_t *buffer);
+
+exception_t decodeARMCBControlInvocation(word_t label, unsigned int length, cptr_t cptr,
+                                         cte_t *srcSlot, cap_t cap, extra_caps_t extraCaps,
+                                         bool_t call, word_t *buffer);
+
+exception_t decodeARMCBInvocation(word_t label, unsigned int length, cptr_t cptr,
+                                  cte_t *srcSlot, cap_t cap, extra_caps_t extraCaps,
+                                  bool_t call, word_t *buffer);
--- src/arch/arm/object/smmu.c
+++ src/arch/arm/object/smmu.c
@@ -4,3 +4,143 @@
  * SPDX-License-Identifier: GPL-2.0-only
  */
 #include <config.h>
+
+#ifdef CONFIG_ARM_SMMU
+#include <arch/object/smmu.h>
+
+exception_t decodeARMSIDControlInvocation(word_t label, unsigned int length, cptr_t cptr,
+	cte_t *srcSlot, cap_t cap, extra_caps_t extraCaps,
+	bool_t call, word_t *buffer) {
+
+	word_t index, depth, sid;
+	cte_t *destSlot;
+	cap_t cnodeCap;
+	lookupSlot_ret_t lu_ret;
+	exception_t status;
+
+	if (label != ARMSIDIssueSIDManager) {
+		userError("SIDControl: Illegal operation.");
+		current_syscall_error.type = seL4_IllegalOperation;
+		return EXCEPTION_SYSCALL_ERROR;
+	}
+	if (length < 3 || extraCaps.excaprefs[0] == NULL) {
+		current_syscall_error.type = seL4_TruncatedMessage;
+		return EXCEPTION_SYSCALL_ERROR;
+	}
+
+	sid = getSyscallArg(0, buffer);
+	index = getSyscallArg(1, buffer);
+	depth = getSyscallArg(2, buffer);
+	cnodeCap = extraCaps.excaprefs[0]->cap;
+
+    if (sid >= SMMU_MAX_SID) {
+        current_syscall_error.type = seL4_RangeError;
+        current_syscall_error.rangeErrorMin = 0;
+        current_syscall_error.rangeErrorMax = SMMU_MAX_SID - 1;
+        userError("Rejecting request for SID %u. SID is greater than or equal to SMMU_MAX_SID.", (int)sid);
+        return EXCEPTION_SYSCALL_ERROR;
+    }
+	if (smmuStateSIDTable[sid]) {
+		current_syscall_error.type = seL4_RevokeFirst;
+		userError("Rejecting request for SID %u. Already active.", (int)sid);
+		return EXCEPTION_SYSCALL_ERROR;
+	}
+
+	lu_ret = lookupTargetSlot(cnodeCap, index, depth);
+	if (lu_ret.status != EXCEPTION_NONE) {
+		userError("Target slot for new SID Handler cap invalid: cap %lu, SID %u.",
+			getExtraCPtr(buffer, 0), (int)sid);
+		return lu_ret.status;
+	}
+	destSlot = lu_ret.slot;
+	status = ensureEmptySlot(destSlot);
+	if (status != EXCEPTION_NONE) {
+		userError("Target slot for new SID Handler cap not empty: cap %lu, SID %u.",
+			getExtraCPtr(buffer, 0), (int)sid);
+		return status;
+	}
+	setThreadState(NODE_STATE(ksCurThread), ThreadState_Restart);
+	smmuStateSIDTable[sid] = true;
+    cteInsert(cap_sid_cap_new(sid), srcSlot, destSlot);
+	return EXCEPTION_NONE;
+}
+
+exception_t decodeARMSIDInvocation(word_t label, unsigned int length, cptr_t cptr,
+	cte_t *srcSlot, cap_t cap, extra_caps_t extraCaps,
+	bool_t call, word_t *buffer) {
+
+	userError("SID: Illegal operation.");
+	current_syscall_error.type = seL4_IllegalOperation;
+	return EXCEPTION_SYSCALL_ERROR;
+}
+
+exception_t decodeARMCBControlInvocation(word_t label, unsigned int length, cptr_t cptr,
+	cte_t *srcSlot, cap_t cap, extra_caps_t extraCaps,
+	bool_t call, word_t *buffer) {
+
+	word_t index, depth, cb;
+	cte_t *destSlot;
+	cap_t cnodeCap;
+	lookupSlot_ret_t lu_ret;
+	exception_t status;
+
+	if (label != ARMCBIssueCBManager) {
+		userError("CBControl: Illegal operation.");
+		current_syscall_error.type = seL4_IllegalOperation;
+		return EXCEPTION_SYSCALL_ERROR;
+	}
+	if (length < 3 || extraCaps.excaprefs[0] == NULL) {
+		current_syscall_error.type = seL4_TruncatedMessage;
+		return EXCEPTION_SYSCALL_ERROR;
+	}
+
+	cb = getSyscallArg(0, buffer);
+	index = getSyscallArg(1, buffer);
+	depth = getSyscallArg(2, buffer);
+	cnodeCap = extraCaps.excaprefs[0]->cap;
+
+    if (cb >= SMMU_MAX_CB) {
+        current_syscall_error.type = seL4_RangeError;
+        current_syscall_error.rangeErrorMin = 0;
+        current_syscall_error.rangeErrorMax = SMMU_MAX_CB - 1;
+        userError("Rejecting request for CB %u. CB is greater than or equal to SMMU_MAX_CB.", (int)cb);
+        return EXCEPTION_SYSCALL_ERROR;
+    }
+	if (smmuStateCBTable[cb]) {
+		current_syscall_error.type = seL4_RevokeFirst;
+		userError("Rejecting request for CB %u. Already active.", (int)cb);
+		return EXCEPTION_SYSCALL_ERROR;
+	}
+
+	lu_ret = lookupTargetSlot(cnodeCap, index, depth);
+	if (lu_ret.status != EXCEPTION_NONE) {
+		userError("Target slot for new SID Handler cap invalid: cap %lu, CB %u.",
+			getExtraCPtr(buffer, 0), (int)cb);
+		return lu_ret.status;
+	}
+	destSlot = lu_ret.slot;
+	status = ensureEmptySlot(destSlot);
+	if (status != EXCEPTION_NONE) {
+		userError("Target slot for new CB Handler cap not empty: cap %lu, CB %u.",
+			getExtraCPtr(buffer, 0), (int)cb);
+		return status;
+	}
+
+	setThreadState(NODE_STATE(ksCurThread), ThreadState_Restart);
+	smmuStateCBTable[cb] = true;
+	cteInsert(cap_cb_cap_new(cb), srcSlot, destSlot);
+	return EXCEPTION_NONE;
+}
+
+exception_t decodeARMCBInvocation(word_t label, unsigned int length, cptr_t cptr,
+	cte_t *srcSlot, cap_t cap, extra_caps_t extraCaps,
+	bool_t call, word_t *buffer) {
+
+	userError("CB: Illegal operation.");
+	current_syscall_error.type = seL4_IllegalOperation;
+	return EXCEPTION_SYSCALL_ERROR;
+
+}
+
+#endif
+
