ef94b463bd43a7e9eadc127432986cd9c1d92500
Gerwin Klein <gerwin.klein@data61.csiro.au>
2020-03-06 17:36:45 +0800

    clarify license for generated headers
    Invocation and syscall headers are generated and provided under
    BSD-2-Clause for user code and GPL-2.0-only for kernel code. To
    facilitate people other than the copyright holder performing this
    operation, the master files are provided under both (BSD or GPL, free
    to choose).

--- tools/syscall_header_gen.py
+++ tools/syscall_header_gen.py
@@ -2,7 +2,7 @@
 #
 # Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
 #
-# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-License-Identifier: BSD-2-Clause or GPL-2.0-only
 #
 
 # seL4 System Call ID Generator
