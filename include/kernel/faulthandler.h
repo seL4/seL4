e06aa34c3a644c0177e1d9d86bbd5061447c9135
Curtis Millar <curtis.millar@data61.csiro.au>
2020-07-23 18:23:52 +1000

    mcs: Don't send fault to unschedulable SC
    This prevents a thread from configuring an endpoint as its timeout
    endpoint when that endpoint is serviced by passive servers. It prevents
    a timeout fault of any kind from being delivered to a server that would
    itself require a fault (but not be delivered due to the recursion
    restriction). This punishes the thread that originally faulted as it has
    an incorrect configuration of its timeout endpoint.
    
    This check is currently particularly expensive, it could potentially be
    made cheaper by encoding the schedulability of the receiver in the
    endpoint state. It may be sufficient to encode whether or not the first
    blocked receiver is schedulable as a distinct (but equivalent) receive
    state on the endpoint, however maintaining this invariant across
    modifications to the first thread blocked on the endpoint would add
    complexity to the implementation. An even cheaper alternative may be
    only encoding whether the first thread was passive when it first
    blocked. This would imply that removing the SC of a thread blocked on an
    endpoint with untrusted clients is a configuration error on the user's
    part.
    
    Signed-off-by: Curtis Millar <curtis.millar@data61.csiro.au>

--- include/kernel/faulthandler.h
+++ include/kernel/faulthandler.h
@@ -7,11 +7,29 @@
 #pragma once
 
 #include <object.h>
+#ifdef CONFIG_KERNEL_MCS
+#include <kernel/sporadic.h>
+#endif
 
 #ifdef CONFIG_KERNEL_MCS
 static inline bool_t validTimeoutHandler(tcb_t *tptr)
 {
-    return cap_get_capType(TCB_PTR_CTE_PTR(tptr, tcbTimeoutHandler)->cap) == cap_endpoint_cap;
+    cap_t handlerCap = TCB_PTR_CTE_PTR(tptr, tcbTimeoutHandler)->cap;
+    if (likely(cap_get_capType(handlerCap) == cap_endpoint_cap)) {
+        endpoint_t *handler = EP_PTR(cap_endpoint_cap_get_capEPPtr(handlerCap));
+        if (endpoint_ptr_get_state(handler) == EPState_Recv) {
+            tcb_queue_t queue = ep_ptr_get_queue(handler);
+            tcb_t *dest = queue.head;
+            /* If the recevier does not have its own valid SC, the
+             * timeout handler is not valid */
+            sched_context_t *sc = dest->tcbSchedContext;
+            return sc != NULL && sc->scRefillMax > 0 && refill_ready(sc);
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 }
 
 void handleTimeout(tcb_t *tptr);
