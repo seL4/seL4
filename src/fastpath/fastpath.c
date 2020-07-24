f127b0bdb5937fe770d909904fd23759c903c1ab
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2020-07-24 13:33:58 +1000

    mcs,fastpath: Check donation in ReplyRecv
    If the current SC isn't going to get donated then this is a slowpath
    operation.
    
    Signed-off-by: Kent McLeod <Kent.Mcleod@data61.csiro.au>

--- src/fastpath/fastpath.c
+++ src/fastpath/fastpath.c
@@ -296,9 +296,11 @@ void NORETURN fastpath_reply_recv(word_t cptr, word_t msgInfo)
 #ifdef CONFIG_KERNEL_MCS
     /* Get the reply address */
     reply_t *reply_ptr = REPLY_PTR(cap_reply_cap_get_capReplyPtr(reply_cap));
-    /* check that its valid and at the head of the call chain */
+    /* check that its valid and at the head of the call chain
+       and that the current thread's SC is going to be donated. */
     if (unlikely(reply_ptr->replyTCB == NULL ||
-                 call_stack_get_isHead(reply_ptr->replyNext) == 0)) {
+                 call_stack_get_isHead(reply_ptr->replyNext) == 0 ||
+                 SC_PTR(call_stack_get_callStackPtr(reply_ptr->replyNext)) != NODE_STATE(ksCurThread)->tcbSchedContext)) {
         slowpath(SysReplyRecv);
     }
 
