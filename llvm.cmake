aa33f87cc5886d545f51e27b1e1061684568d8d7
Stefan O'Rear <sorear@fastmail.com>
2020-08-01 23:51:39 -0400

    riscv/clang: Use integrated as
    When not using integrated as, clang generates %got_pcrel_hi relocs which
    are not supported by any currently released version of binutils.
    
    Signed-off-by: Stefan O'Rear <sorear@fastmail.com>

--- llvm.cmake
+++ llvm.cmake
@@ -35,7 +35,9 @@ set(CMAKE_CXX_COMPILER_TARGET ${TRIPLE})
 
 string(APPEND c_common_flags " -Qunused-arguments")
 string(APPEND c_common_flags " -Wno-constant-logical-operand")
-string(APPEND c_common_flags " -fno-integrated-as")
+if(NOT ("${TRIPLE}" MATCHES "^riscv"))
+    string(APPEND c_common_flags " -fno-integrated-as")
+endif()
 # clang 11 has a regression in GlobalISel (only used at -O0) affecting the syscall
 # stubs in libsel4runtime; see https://reviews.llvm.org/D83384#2189132
 string(APPEND c_common_flags " -fno-experimental-isel")
