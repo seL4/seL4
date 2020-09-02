b77c9b205193470b1802591d5ac8141d7c093b56
Jesse Millwood <jesse.millwood@dornerworks.com>
2020-09-02 09:07:07 -0400

    PolarFire SoC: Initial support for platform
    Signed-off-by: Jesse Millwood <jesse.millwood@dornerworks.com>

---
+++ libsel4/sel4_plat_include/polarfire/sel4/plat/api/constants.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2020, DornerWorks
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#ifdef HAVE_AUTOCONF
+#include <autoconf.h>
+#endif
---
+++ src/plat/polarfire/config.cmake
@@ -0,0 +1,27 @@
+#
+# Copyright 2020, DornerWorks
+#
+# This software may be distributed and modified according to the terms of
+# the GNU General Public License version 2. Note that NO WARRANTY is provided.
+# See "LICENSE_GPLv2.txt" for details.
+#
+# @TAG(DORNERWORKS_GPL)
+#
+
+cmake_minimum_required(VERSION 3.7.2)
+
+declare_platform(polarfire KernelPlatformPolarfire PLAT_POLARFIRE KernelSel4ArchRiscV64)
+
+if(KernelPlatformPolarfire)
+    declare_seL4_arch(riscv64)
+    config_set(KernelRiscVPlatform RISCV_PLAT "polarfire")
+    config_set(KernelPlatformFirstHartID FIRST_HART_ID 1)
+    list(APPEND KernelDTSList "tools/dts/mpfs_icicle.dts")
+    list(APPEND KernelDTSList "src/plat/polarfire/overlay-polarfire.dts")
+    declare_default_headers(
+        TIMER_FREQUENCY 10000000llu PLIC_MAX_NUM_INT 186
+        INTERRUPT_CONTROLLER drivers/irq/riscv_plic0.h
+    )
+else()
+    unset(KernelPlatformFirstHartID CACHE)
+endif()
