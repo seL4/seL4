5c1b81d9bb2db0e5f7801ce3442b8a18f96b6001
Gerwin Klein <gerwin.klein@data61.csiro.au>
2020-09-04 12:29:26 +0800

    libsel4: fix license tags
    These files should have been released under BSD-2-Clause in the first
    place (as per parent LICENSE.md file).
    
    Closes #245
    
    Signed-off-by: Gerwin Klein <gerwin.klein@data61.csiro.au>

--- libsel4/arch_include/riscv/sel4/arch/syscalls.h
+++ libsel4/arch_include/riscv/sel4/arch/syscalls.h
@@ -2,7 +2,7 @@
  * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
  * Copyright 2015, 2016 Hesham Almatary <heshamelmatary@gmail.com>
  *
- * SPDX-License-Identifier: GPL-2.0-only
+ * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #pragma once
