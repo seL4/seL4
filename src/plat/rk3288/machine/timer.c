ab6e39082dac0f21d76fb89ad370b5705ad514eb
Peter Chubb <Peter.Chubb@data61.csiro.au>
2020-11-10 20:35:05 +1100

    Add SPDX licence rk3288
    The timer.c file is copied from Linux and is GPL2.0
    
    Signed-off-by: Peter Chubb <peter.chubb@data61.csiro.au>

--- src/plat/rk3288/machine/timer.c
+++ src/plat/rk3288/machine/timer.c
@@ -1,3 +1,13 @@
+/*
+ * Copyright 2020 Data61, CSIRO (ABN 41 687 119 230)
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
+/*
+ * This code was taken from the Linux kernel, file arch/arm/mach-rockchip/rockchip.c
+ */
+
 #define RK3288_TIMER6_7_PHYS 0xff810000
 
 #define writel(v, a) (*(uint32_t *)(a) = (v))
