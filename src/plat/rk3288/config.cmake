0d2331bebb8df17f77f316d71e7a17a25d6152f6
Peter Chubb <Peter.Chubb@data61.csiro.au>
2020-11-23 15:43:06 +1100

    Clean up cmake config for rk3288
    Try to simplify as suggested by review comments.
    
    Signed-off-by: Peter Chubb <peter.chubb@data61.csiro.au>

--- src/plat/rk3288/config.cmake
+++ src/plat/rk3288/config.cmake
@@ -12,20 +12,13 @@ cmake_minimum_required(VERSION 3.7.2)
 # easily `grep` a list of supported platforms.
 # For now we don't support HYP... but include it for later.
 set(AArch32OrArchArmHyp "KernelSel4ArchAarch32 OR KernelSel4ArchArmHyp")
-declare_platform(rk3288 KernelPlatformRK3288 PLAT_RK3288 ${AArch32OrArchArmHyp})
+declare_platform(rk3288 KernelPlatRK3288 PLAT_RK3288 ${AArch32OrArchArmHyp})
 unset(${AArch32OrArchArmHyp} CACHE)
 
-set(cmake_configs KernelPlatformRK3288NTablet)
-set(c_configs PLAT_RK3288_NTABLET)
-set(plat_lists rk3288-ntablet)
-foreach(config IN LISTS cmake_configs)
-    unset(${config} CACHE)
-endforeach()
-
-if(KernelPlatformRK3288)
-    #("${KernelSel4Arch}" STREQUAL aarch32)
+if(KernelPlatRK3288)
+    #if("${KernelSel4Arch}" STREQUAL aarch32)
     declare_seL4_arch(aarch32)
-    #("${KernelSel4Arch}" STREQUAL arm_hyp)
+    #elif("${KernelSel4Arch}" STREQUAL arm_hyp)
     #    declare_seL4_arch(arm_hyp)
     #else()
     #    fallback_declare_seL4_arch_default(aarch32)
@@ -34,24 +27,16 @@ if(KernelPlatformRK3288)
     set(KernelArchArmV7ve ON)
     # v7ve is a superset of v7a, so we enable that as well
     set(KernelArchArmV7a ON)
-    config_set(KernelArmMach MACH "rk3288")
-    check_platform_and_fallback_to_default(KernelARMPlatform "rk3288-ntablet")
+    config_set(KernelARMPlatform ARM_PLAT rk3288)
+    config_set(KernelArmMach "rk3288" CACHE INTERNAL "")
 
-    list(FIND plat_lists "${KernelARMPlatform}" index)
-    if("${index}" STREQUAL "-1")
-        message(FATAL_ERROR "Invalid rk3288 platform selected: \"${KernelARMPlatform}\"")
-    endif()
-    list(GET c_configs ${index} c_config)
-    list(GET cmake_configs ${index} cmake_config)
-    config_set(KernelARMPlatform ARM_PLAT ${KernelARMPlatform})
-    config_set(${cmake_config} ${c_config} ON)
+    list(APPEND KernelDTSList "tools/dts/${KernelARMPlatform}-ntablet.dts")
+    list(APPEND KernelDTSList "src/plat/rk3288/overlay-${KernelARMPlatform}-ntablet.dts")
 
-    list(APPEND KernelDTSList "tools/dts/${KernelARMPlatform}.dts")
-    list(APPEND KernelDTSList "src/plat/rk3288/overlay-${KernelARMPlatform}.dts")
     declare_default_headers(
         TIMER_FREQUENCY 24000000llu
         MAX_IRQ 186
-        NUM_PPI 4
+        NUM_PPI 32
         TIMER drivers/timer/arm_generic.h
         INTERRUPT_CONTROLLER arch/machine/gic_v2.h
         CLK_MAGIC 2863311531llu
@@ -61,7 +46,7 @@ if(KernelPlatformRK3288)
 endif()
 
 add_sources(
-    DEP "KernelPlatformRK3288"
+    DEP "KernelPlatRK3288"
     CFILES
         # I consider the next line to be too long,
         # but the github style checker insists on having it all on one line.
