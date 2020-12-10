seL4/seL4

commit 454dfd897fb9b3470b42bae2290c91fb5ed5da39

Handle read/write of VCPU CPACR register correctly
The CPACR register should not be directly read/written to hardware
unless the VCPU is currently active. CPACR is effectively managed
by the kernel for non-VCPU TCBs, so writing it directly causes
problems. Instead write to (read from) the shadow. It will be
written to hardware register on a context switch.

Note: Logic for reading/writing CPACR register on context switch
is already implemented in vcpu_disable/vcpu_enable functions. This
is just making the direct read/write consistent with that existing
implementation.

See: SELFOUR-2821
Signed-off-by: Ben Leslie <benno@brkawy.com>

src/arch/arm/object/vcpu.c
@@ -67,6 +67,14 @@ static word_t readVCPUReg(vcpu_t *vcpu, word_t field)
             } else {
                 return vcpu_read_reg(vcpu, field);
             }
+#ifdef CONFIG_HAVE_FPU
+        case seL4_VCPUReg_CPACR:
+            if (ARCH_NODE_STATE(armHSVCPUActive)) {
+                return vcpu_hw_read_reg(field);
+            } else {
+                return vcpu_read_reg(vcpu, field);
+            }
+#endif
         default:
             return vcpu_hw_read_reg(field);
         }
@@ -86,6 +94,15 @@ static void writeVCPUReg(vcpu_t *vcpu, word_t field, word_t value)
                 vcpu_write_reg(vcpu, field, value);
             }
             break;
+#ifdef CONFIG_HAVE_FPU
+        case seL4_VCPUReg_CPACR:
+            if (ARCH_NODE_STATE(armHSVCPUActive)) {
+                vcpu_hw_write_reg(field, value);
+            } else {
+                vcpu_write_reg(vcpu, field, value);
+            }
+            break;
+#endif
         default:
             vcpu_hw_write_reg(field, value);
         }

commit 93ab2543d9d8a84d1d5a66c4f8883bcbac35c009

arm: Generalise vcpu fields saved when inactive
Different microarchitectures & configurations save different sets of
VCPU registers when the current VCPU is not active. This generalises the
handling of these cases such that each microarchitecture defines which
regisers are managed in this fashion.

Fixes regression introduced in 454dfd897fb9b3470b42bae2290c91fb5ed5da39

Signed-off-by: Curtis Millar <curtis.millar@data61.csiro.au>

include/arch/arm/armv/armv7-a/armv/vcpu.h
@@ -850,6 +850,15 @@ static inline bool_t armv_handleVCPUFault(word_t hsr)
     return false;
 }
 
+static inline bool_t vcpu_reg_saved_when_disabled(word_t field)
+{
+    switch (field) {
+    case seL4_VCPUReg_SCTLR:
+        return true;
+    default:
+        return false;
+    }
+}
 
 #endif /* End of CONFIG_ARM_HYPERVISOR_SUPPORT */
 

include/arch/arm/armv/armv8-a/64/armv/vcpu.h
@@ -660,6 +660,19 @@ static inline bool_t armv_handleVCPUFault(word_t hsr)
     return false;
 }
 
+static inline bool_t vcpu_reg_saved_when_disabled(word_t field)
+{
+    switch (field) {
+    case seL4_VCPUReg_SCTLR:
+#ifdef CONFIG_HAVE_FPU
+    case seL4_VCPUReg_CPACR:
+#endif
+        return true;
+    default:
+        return false;
+    }
+}
+
 #endif /* End of CONFIG_ARM_HYPERVISOR_SUPPORT */
 
 

src/arch/arm/object/vcpu.c
@@ -58,24 +58,9 @@ static void vcpu_save(vcpu_t *vcpu, bool_t active)
 static word_t readVCPUReg(vcpu_t *vcpu, word_t field)
 {
     if (likely(ARCH_NODE_STATE(armHSCurVCPU) == vcpu)) {
-        switch (field) {
-        case seL4_VCPUReg_SCTLR:
-            /* The SCTLR value is switched to/from hardware when we enable/disable
-             * the vcpu, not when we switch vcpus */
-            if (ARCH_NODE_STATE(armHSVCPUActive)) {
-                return getSCTLR();
-            } else {
-                return vcpu_read_reg(vcpu, field);
-            }
-#ifdef CONFIG_HAVE_FPU
-        case seL4_VCPUReg_CPACR:
-            if (ARCH_NODE_STATE(armHSVCPUActive)) {
-                return vcpu_hw_read_reg(field);
-            } else {
-                return vcpu_read_reg(vcpu, field);
-            }
-#endif
-        default:
+        if (vcpu_reg_saved_when_disabled(field) && !ARCH_NODE_STATE(armHSVCPUActive)) {
+            return vcpu_read_reg(vcpu, field);
+        } else {
             return vcpu_hw_read_reg(field);
         }
     } else {
@@ -86,24 +71,9 @@ static word_t readVCPUReg(vcpu_t *vcpu, word_t field)
 static void writeVCPUReg(vcpu_t *vcpu, word_t field, word_t value)
 {
     if (likely(ARCH_NODE_STATE(armHSCurVCPU) == vcpu)) {
-        switch (field) {
-        case seL4_VCPUReg_SCTLR:
-            if (ARCH_NODE_STATE(armHSVCPUActive)) {
-                setSCTLR(value);
-            } else {
-                vcpu_write_reg(vcpu, field, value);
-            }
-            break;
-#ifdef CONFIG_HAVE_FPU
-        case seL4_VCPUReg_CPACR:
-            if (ARCH_NODE_STATE(armHSVCPUActive)) {
-                vcpu_hw_write_reg(field, value);
-            } else {
-                vcpu_write_reg(vcpu, field, value);
-            }
-            break;
-#endif
-        default:
+        if (vcpu_reg_saved_when_disabled(field) && !ARCH_NODE_STATE(armHSVCPUActive)) {
+            vcpu_write_reg(vcpu, field, value);
+        } else {
             vcpu_hw_write_reg(field, value);
         }
     } else {