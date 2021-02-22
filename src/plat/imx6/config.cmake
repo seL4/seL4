d486bc0b5a9bc5e05f195220959497acc0fedba1
Axel Heider <axel.heider@hensoldt-cyber.de>
2021-02-22 19:26:55 +0100

    Add i.MX6 Nitrogen6_SoloX board
    Signed-off-by: Axel Heider <axel.heider@hensoldt-cyber.de>

--- src/plat/imx6/config.cmake
+++ src/plat/imx6/config.cmake
@@ -1,5 +1,6 @@
 #
 # Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+# Copyright 2020, HENSOLDT Cyber GmbH
 #
 # SPDX-License-Identifier: GPL-2.0-only
 #
@@ -8,26 +9,47 @@ cmake_minimum_required(VERSION 3.7.2)
 
 declare_platform(imx6 KernelPlatImx6 PLAT_IMX6 KernelSel4ArchAarch32)
 
-set(c_configs PLAT_SABRE PLAT_WANDQ)
-set(cmake_configs KernelPlatformSabre KernelPlatformWandQ)
-set(plat_lists sabre wandq)
-foreach(config IN LISTS cmake_configs)
-    unset(${config} CACHE)
+# disable platform specific settings by default in cache, will be enabled below
+# if active
+foreach(
+    var
+    IN
+    ITEMS
+    KernelPlatformSabre
+    KernelPlatformWandQ
+    KernelPlatformNitrogen6SX
+    KernelPlatImx6dq
+    KernelPlatImx6sx
+)
+    unset(${var} CACHE)
+    set(${var} OFF)
 endforeach()
+
 if(KernelPlatImx6)
+
+    check_platform_and_fallback_to_default(KernelARMPlatform "sabre")
+
+    if(KernelARMPlatform STREQUAL "sabre")
+        config_set(KernelPlatformSabre PLAT_SABRE ON)
+        config_set(KernelPlatImx6dq PLAT_IMX6DQ ON)
+
+    elseif(KernelARMPlatform STREQUAL "wandq")
+        config_set(KernelPlatformWandQ PLAT_WANDQ ON)
+        config_set(KernelPlatImx6dq PLAT_IMX6DQ ON)
+
+    elseif(KernelARMPlatform STREQUAL "nitrogen6sx")
+        config_set(KernelPlatformNitrogen6SX PLAT_NITROGENSX ON)
+        config_set(KernelPlatImx6sx PLAT_IMX6SX ON)
+
+    else()
+        message(FATAL_ERROR "Which imx6 platform not specified")
+    endif()
+
+    config_set(KernelARMPlatform ARM_PLAT ${KernelARMPlatform})
     declare_seL4_arch(aarch32)
     set(KernelArmCortexA9 ON)
     set(KernelArchArmV7a ON)
     set(KernelArmMach "imx" CACHE INTERNAL "")
-    check_platform_and_fallback_to_default(KernelARMPlatform "sabre")
-    list(FIND plat_lists ${KernelARMPlatform} index)
-    if("${index}" STREQUAL "-1")
-        message(FATAL_ERROR "Which imx6 platform not specified")
-    endif()
-    list(GET c_configs ${index} c_config)
-    list(GET cmake_configs ${index} cmake_config)
-    config_set(KernelARMPlatform ARM_PLAT ${KernelARMPlatform})
-    config_set(${cmake_config} ${c_config} ON)
     list(APPEND KernelDTSList "tools/dts/${KernelARMPlatform}.dts")
     list(APPEND KernelDTSList "src/plat/imx6/overlay-${KernelARMPlatform}.dts")
 
