138e809e7252912899de9f7ab3ac8bd7a85e33f2
Axel Heider <axelheider@gmx.de>
2021-06-04 03:09:49 +0200

    use const qualifier
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- include/machine.h
+++ include/machine.h
@@ -21,28 +21,28 @@ static inline void *CONST ptrFromPAddr(paddr_t paddr)
 
 /* When obtaining a physical address from a reference to any object in
  * the physical mapping window, this function must be used. */
-static inline paddr_t CONST addrFromPPtr(void *pptr)
+static inline paddr_t CONST addrFromPPtr(const void *pptr)
 {
     return (paddr_t)pptr - PPTR_BASE_OFFSET;
 }
 
 /* When obtaining a physical address from a reference to an address from
  * the kernel ELF mapping, this function must be used. */
-static inline paddr_t CONST addrFromKPPtr(void *pptr)
+static inline paddr_t CONST addrFromKPPtr(const void *pptr)
 {
     assert((paddr_t)(pptr) >= KERNEL_ELF_BASE);
     assert((paddr_t)(pptr) <= KERNEL_ELF_TOP);
     return (paddr_t)pptr - KERNEL_ELF_BASE_OFFSET;
 }
 
-static inline region_t CONST paddr_to_pptr_reg(p_region_t p_reg)
+static inline region_t CONST paddr_to_pptr_reg(const p_region_t p_reg)
 {
     return (region_t) {
         p_reg.start + PPTR_BASE_OFFSET, p_reg.end + PPTR_BASE_OFFSET
     };
 }
 
-static inline p_region_t CONST pptr_to_paddr_reg(region_t reg)
+static inline p_region_t CONST pptr_to_paddr_reg(const region_t reg)
 {
     return (p_region_t) {
         reg.start - PPTR_BASE_OFFSET, reg.end - PPTR_BASE_OFFSET
