dc9ec49cf2554b50e054459ef2165109339f7366
Miki Tanaka <miki.tanaka@data61.csiro.au>
2021-06-30 19:28:09 +1000

    move scSporadic update
    - in decodeSchedControl_ConfigureFlags
    - a minor tweak to aid verification.
    
    Signed-off-by: Miki Tanaka <miki.tanaka@data61.csiro.au>

--- src/object/schedcontrol.c
+++ src/object/schedcontrol.c
@@ -15,6 +15,7 @@ static exception_t invokeSchedControl_ConfigureFlags(sched_context_t *target, wo
 {
 
     target->scBadge = badge;
+    target->scSporadic = (flags & seL4_SchedContext_Sporadic) != 0;
 
     /* don't modify parameters of tcb while it is in a sorted queue */
     if (target->scTcb) {
@@ -41,8 +42,6 @@ static exception_t invokeSchedControl_ConfigureFlags(sched_context_t *target, wo
 #endif /* ENABLE_SMP_SUPPORT */
         }
     }
-
-    target->scSporadic = (flags & seL4_SchedContext_Sporadic) != 0;
 
     if (budget == period) {
         /* this is a cool hack: for round robin, we set the
