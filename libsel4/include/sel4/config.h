c7d5bb0ed4ac7d8efa0a9b94426afe96d917b77e
Kent McLeod <kent@kry10.com>
2021-07-09 12:05:22 +1000

    libsel4: Fix name for AARCH64_VSPACE_S2_START_L1
    CONFIG_AARCH64_VSPACE_S2_START_L1 has the correct namespace for a kernel
    config option.
    
    Signed-off-by: Kent McLeod <kent@kry10.com>

--- libsel4/include/sel4/config.h
+++ libsel4/include/sel4/config.h
@@ -10,12 +10,6 @@
 
 #include <autoconf.h>
 
-#ifdef CONFIG_ARM_HYPERVISOR_SUPPORT
-#ifdef CONFIG_ARM_PA_SIZE_BITS_40
-#define AARCH64_VSPACE_S2_START_L1
-#endif
-#endif
-
 /* Configurations requring the kernel log buffer */
 #if defined CONFIG_BENCHMARK_TRACK_KERNEL_ENTRIES || \
     defined CONFIG_BENCHMARK_TRACEPOINTS
