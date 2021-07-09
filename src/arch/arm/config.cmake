b762ba78479e007f67ea47ca720822a5dc7978f2
Kent McLeod <kent@kry10.com>
2021-07-09 12:05:22 +1000

    libsel4: Fix name for AARCH64_VSPACE_S2_START_L1
    CONFIG_AARCH64_VSPACE_S2_START_L1 has the correct namespace for a kernel
    config option.
    
    Signed-off-by: Kent McLeod <kent@kry10.com>

--- src/arch/arm/config.cmake
+++ src/arch/arm/config.cmake
@@ -96,6 +96,12 @@ config_option(
         "KernelArmCortexA15 OR KernelArmCortexA35 OR KernelArmCortexA57 OR KernelArmCortexA53 OR KernelArmCortexA72"
 )
 
+if(KernelArmPASizeBits40 AND ARM_HYPERVISOR_SUPPORT)
+    config_set(KernelAarch64VspaceS2StartL1 AARCH64_VSPACE_S2_START_L1 "ON")
+else()
+    config_set(KernelAarch64VspaceS2StartL1 AARCH64_VSPACE_S2_START_L1 "OFF")
+endif()
+
 config_option(
     KernelArmHypEnableVCPUCP14SaveAndRestore ARM_HYP_ENABLE_VCPU_CP14_SAVE_AND_RESTORE
     "Trap, but don't save/restore VCPUs' CP14 accesses \
