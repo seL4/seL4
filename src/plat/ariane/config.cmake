4081010f5b7ad559954c69c472044661a3aac45a
Axel Heider <axel.heider@hensoldt-cyber.de>
2021-07-24 23:53:09 +0200

    risc-v: use dummy PLIC driver on ariane/rocketchip
    Both Ariane and Rocketchip are experimental platforms that support
    accessing peripherals, but the status of support for external interrupts
    via a PLIC is unclear and my differ depending on the version that is
    synthesized. Using the dummy PLIC driver seems the better option for now
    to avoid confusion or even crashes.
    
    Signed-off-by: Axel Heider <axel.heider@hensoldt-cyber.de>

--- src/plat/ariane/config.cmake
+++ src/plat/ariane/config.cmake
@@ -1,5 +1,6 @@
 #
 # Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+# Copyright 2021, HENSOLDT Cyber
 #
 # SPDX-License-Identifier: GPL-2.0-only
 #
@@ -15,9 +16,14 @@ if(KernelPlatformAriane)
     config_set(KernelOpenSBIPlatform OPENSBI_PLATFORM "fpga/ariane")
     list(APPEND KernelDTSList "tools/dts/ariane.dts")
     list(APPEND KernelDTSList "src/plat/ariane/overlay-ariane.dts")
+    # This is an experimental platform that supports accessing peripherals, but
+    # the status of support for external interrupts via a PLIC is unclear and
+    # may differ depending on the version that is synthesized. Declaring no
+    # interrupts and using the dummy PLIC driver seems the best option for now
+    # to avoid confusion or even crashes.
     declare_default_headers(
-        TIMER_FREQUENCY 25000000 PLIC_MAX_NUM_INT 30
-        INTERRUPT_CONTROLLER drivers/irq/riscv_plic0.h
+        TIMER_FREQUENCY 25000000 PLIC_MAX_NUM_INT 0
+        INTERRUPT_CONTROLLER drivers/irq/riscv_plic_dummy.h
     )
 else()
     unset(KernelPlatformFirstHartID CACHE)
