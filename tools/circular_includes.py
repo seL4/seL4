560f215489718084b41827032a663ff9ac66a0a9
Alexander Fasching <fasching.a91@gmail.com>
2021-09-03 04:54:17 +0200

    tools: Fix regex for ignore pattern (#535)
    Signed-off-by: Alexander Fasching <fasching.a91@gmail.com>

--- tools/circular_includes.py
+++ tools/circular_includes.py
@@ -28,7 +28,7 @@ def main(parse_args):
     if ignore_args and len(ignore_args):
         ignore_args = [re.escape(ignore) for ignore in ignore_args]
         ignore_re_string = '(' + '|'.join(ignore_args) + ')'
-        ignore_re = re.compile(r'^# 1 ".*' + ignore_re_string + '"')
+        ignore_re = re.compile(r'^# [01] ".*' + ignore_re_string + '"')
 
     header_re = re.compile(r'^# (\d+) "(.*\..)"')
 
