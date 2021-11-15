9db040fbaa3b0182b97916a8ffeb8f0ba0b05065
Axel Heider <axelheider@gmx.de>
2021-11-15 04:09:59 +0100

    unify libsel4 type definitions
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- include/arch/riscv/arch/types.h
+++ include/arch/riscv/arch/types.h
@@ -26,15 +26,6 @@ typedef word_t cpu_id_t;
 typedef word_t node_id_t;
 typedef word_t dom_t;
 
-/* for libsel4 headers that the kernel shares */
-typedef word_t seL4_Word;
-typedef cptr_t seL4_CPtr;
-typedef uint32_t seL4_Uint32;
-typedef uint8_t seL4_Uint8;
-typedef node_id_t seL4_NodeId;
-typedef paddr_t seL4_PAddr;
-typedef dom_t seL4_Domain;
-
 typedef uint64_t timestamp_t;
 
 #define wordBits BIT(wordRadix)
