4d7fc32a242c1a9d607467dab56d5ccc971554cf
Ben Leslie <benno@brkawy.com>
2021-11-16 01:49:36 +0000

    Remove python2 support for core build tools
    Depedencies on Python2/3 cross support (such as six, past and future)
    are removed as only Python3 is supported at this point. Fewer
    external deps is a good thing.
    
    Signed-off-by: Ben Leslie <benno@brkawy.com>
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- tools/umm.py
+++ tools/umm.py
@@ -5,9 +5,7 @@
 # SPDX-License-Identifier: BSD-2-Clause
 #
 
-from __future__ import print_function
 import sys
-import six
 from functools import reduce
 
 # We assume length tp > 0
@@ -119,7 +117,7 @@ def print_graph(filename, out_file):
     mp = build_types(filename)
 
     print('digraph types {', file=out_file)
-    for k, flds in six.iteritems(mp):
+    for k, flds in mp.iteritems():
         for fld, tp in flds:
             # if is_base(tp):
             print('\t "%s" -> "%s" [label="%s"]' % (k, base_name(tp), fld),
