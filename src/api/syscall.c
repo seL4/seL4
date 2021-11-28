d0e313bff21c286e92861781249c8988e4b4a469
Axel Heider <axelheider@gmx.de>
2021-11-28 10:03:55 +0100

    use wrapper function for benchmark syscalls
    Factoring out the benchmark syscall handling in dedicated functions
    improves code readability and maintainability.
    
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- src/api/syscall.c
+++ src/api/syscall.c
@@ -185,124 +185,32 @@ exception_t handleUnknownSyscall(word_t w)
 #endif
 
 #ifdef CONFIG_ENABLE_BENCHMARKS
-    if (w == SysBenchmarkFlushCaches) {
-#ifdef CONFIG_ARCH_ARM
-        tcb_t *thread = NODE_STATE(ksCurThread);
-        if (getRegister(thread, capRegister)) {
-            arch_clean_invalidate_L1_caches(getRegister(thread, msgInfoRegister));
-        } else {
-            arch_clean_invalidate_caches();
-        }
-#else
-        arch_clean_invalidate_caches();
-#endif
-        return EXCEPTION_NONE;
-    } else if (w == SysBenchmarkResetLog) {
-#ifdef CONFIG_KERNEL_LOG_BUFFER
-        if (ksUserLogBuffer == 0) {
-            userError("A user-level buffer has to be set before resetting benchmark.\
-                    Use seL4_BenchmarkSetLogBuffer\n");
-            setRegister(NODE_STATE(ksCurThread), capRegister, seL4_IllegalOperation);
-            return EXCEPTION_SYSCALL_ERROR;
-        }
-
-        ksLogIndex = 0;
-#endif /* CONFIG_KERNEL_LOG_BUFFER */
+    switch (w) {
+    case SysBenchmarkFlushCaches:
+        return handle_SysBenchmarkFlushCaches();
+    case SysBenchmarkResetLog:
+        return handle_SysBenchmarkResetLog();
+    case SysBenchmarkFinalizeLog:
+        return handle_SysBenchmarkFinalizeLog();
+    case SysBenchmarkSetLogBuffer:
+        return handle_SysBenchmarkSetLogBuffer();
 #ifdef CONFIG_BENCHMARK_TRACK_UTILISATION
-        NODE_STATE(benchmark_log_utilisation_enabled) = true;
-        benchmark_track_reset_utilisation(NODE_STATE(ksIdleThread));
-        NODE_STATE(ksCurThread)->benchmark.schedule_start_time = ksEnter;
-        NODE_STATE(ksCurThread)->benchmark.number_schedules++;
-
-        NODE_STATE(benchmark_start_time) = ksEnter;
-        NODE_STATE(benchmark_kernel_time) = 0;
-        NODE_STATE(benchmark_kernel_number_entries) = 0;
-        NODE_STATE(benchmark_kernel_number_schedules) = 1;
-        benchmark_arch_utilisation_reset();
-#endif /* CONFIG_BENCHMARK_TRACK_UTILISATION */
-        setRegister(NODE_STATE(ksCurThread), capRegister, seL4_NoError);
-        return EXCEPTION_NONE;
-    } else if (w == SysBenchmarkFinalizeLog) {
-#ifdef CONFIG_KERNEL_LOG_BUFFER
-        ksLogIndexFinalized = ksLogIndex;
-        setRegister(NODE_STATE(ksCurThread), capRegister, ksLogIndexFinalized);
-#endif /* CONFIG_KERNEL_LOG_BUFFER */
-#ifdef CONFIG_BENCHMARK_TRACK_UTILISATION
-        benchmark_utilisation_finalise();
-#endif /* CONFIG_BENCHMARK_TRACK_UTILISATION */
-        return EXCEPTION_NONE;
-    } else if (w == SysBenchmarkSetLogBuffer) {
-#ifdef CONFIG_KERNEL_LOG_BUFFER
-        word_t cptr_userFrame = getRegister(NODE_STATE(ksCurThread), capRegister);
-
-        if (benchmark_arch_map_logBuffer(cptr_userFrame) != EXCEPTION_NONE) {
-            setRegister(NODE_STATE(ksCurThread), capRegister, seL4_IllegalOperation);
-            return EXCEPTION_SYSCALL_ERROR;
-        }
-#endif /* CONFIG_KERNEL_LOG_BUFFER */
-        setRegister(NODE_STATE(ksCurThread), capRegister, seL4_NoError);
-        return EXCEPTION_NONE;
-    }
-
-#ifdef CONFIG_BENCHMARK_TRACK_UTILISATION
-    else if (w == SysBenchmarkGetThreadUtilisation) {
-        benchmark_track_utilisation_dump();
-        return EXCEPTION_NONE;
-    } else if (w == SysBenchmarkResetThreadUtilisation) {
-        word_t tcb_cptr = getRegister(NODE_STATE(ksCurThread), capRegister);
-        lookupCap_ret_t lu_ret;
-        word_t cap_type;
-
-        lu_ret = lookupCap(NODE_STATE(ksCurThread), tcb_cptr);
-        /* ensure we got a TCB cap */
-        cap_type = cap_get_capType(lu_ret.cap);
-        if (cap_type != cap_thread_cap) {
-            userError("SysBenchmarkResetThreadUtilisation: cap is not a TCB, halting");
-            return EXCEPTION_NONE;
-        }
-
-        tcb_t *tcb = TCB_PTR(cap_thread_cap_get_capTCBPtr(lu_ret.cap));
-
-        benchmark_track_reset_utilisation(tcb);
-        return EXCEPTION_NONE;
-    }
+    case SysBenchmarkGetThreadUtilisation:
+        return handle_SysBenchmarkGetThreadUtilisation();
+    case SysBenchmarkResetThreadUtilisation:
+        return handle_SysBenchmarkResetThreadUtilisation();
 #ifdef CONFIG_DEBUG_BUILD
-    else if (w == SysBenchmarkDumpAllThreadsUtilisation) {
-        printf("{\n");
-        printf("  \"BENCHMARK_TOTAL_UTILISATION\":%lu,\n",
-               (word_t)(NODE_STATE(benchmark_end_time) - NODE_STATE(benchmark_start_time)));
-        printf("  \"BENCHMARK_TOTAL_KERNEL_UTILISATION\":%lu,\n", (word_t) NODE_STATE(benchmark_kernel_time));
-        printf("  \"BENCHMARK_TOTAL_NUMBER_KERNEL_ENTRIES\":%lu,\n", (word_t) NODE_STATE(benchmark_kernel_number_entries));
-        printf("  \"BENCHMARK_TOTAL_NUMBER_SCHEDULES\":%lu,\n", (word_t) NODE_STATE(benchmark_kernel_number_schedules));
-        printf("  \"BENCHMARK_TCB_\": [\n");
-        for (tcb_t *curr = NODE_STATE(ksDebugTCBs); curr != NULL; curr = TCB_PTR_DEBUG_PTR(curr)->tcbDebugNext) {
-            printf("    {\n");
-            printf("      \"NAME\":\"%s\",\n", TCB_PTR_DEBUG_PTR(curr)->tcbName);
-            printf("      \"UTILISATION\":%lu,\n", (word_t) curr->benchmark.utilisation);
-            printf("      \"NUMBER_SCHEDULES\":%lu,\n", (word_t) curr->benchmark.number_schedules);
-            printf("      \"KERNEL_UTILISATION\":%lu,\n", (word_t) curr->benchmark.kernel_utilisation);
-            printf("      \"NUMBER_KERNEL_ENTRIES\":%lu\n", (word_t) curr->benchmark.number_kernel_entries);
-            printf("    }");
-            if (TCB_PTR_DEBUG_PTR(curr)->tcbDebugNext != NULL) {
-                printf(",\n");
-            } else {
-                printf("\n");
-            }
-        }
-        printf("  ]\n}\n");
-        return EXCEPTION_NONE;
-    } else if (w == SysBenchmarkResetAllThreadsUtilisation) {
-        for (tcb_t *curr = NODE_STATE(ksDebugTCBs); curr != NULL; curr = TCB_PTR_DEBUG_PTR(curr)->tcbDebugNext) {
-            benchmark_track_reset_utilisation(curr);
-        }
-        return EXCEPTION_NONE;
-    }
+    case SysBenchmarkDumpAllThreadsUtilisation:
+        return handle_SysBenchmarkDumpAllThreadsUtilisation();
+    case SysBenchmarkResetAllThreadsUtilisation:
+        return handle_SysBenchmarkResetAllThreadsUtilisation();
 #endif /* CONFIG_DEBUG_BUILD */
 #endif /* CONFIG_BENCHMARK_TRACK_UTILISATION */
-
-    else if (w == SysBenchmarkNullSyscall) {
+    case SysBenchmarkNullSyscall:
         return EXCEPTION_NONE;
-    }
+    default:
+        break; /* syscall is not for benchmarking */
+    } /* end switch(w) */
 #endif /* CONFIG_ENABLE_BENCHMARKS */
 
     MCS_DO_IF_BUDGET({
