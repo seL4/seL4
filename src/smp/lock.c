d8b49c7c5b6bfd222c9ddb065bfc50e3cb5e480b
Indan Zupancic <Indan.Zupancic@mep-info.com>
2022-06-14 18:53:45 +0200

    SMP: Cleanup kernel lock
    Remove cpu argument from clh_lock_acquire/release, it is never
    valid to call them with something else than getCurrentCPUIndex().
    
    Use clh_is_self_in_queue() instead of poking into lock internals
    in ipiStallCoreCallback().
    
    Add some comments from the paper to make it easier to match the
    implementation with the description in the paper it's based on.
    Don't use confusingly different naming, but follow naming from
    the paper: Rename 'node' to 'myreq' and 'next' to 'watch', etc.
    
    Signed-off-by: Indan Zupancic <Indan.Zupancic@mep-info.com>

--- src/smp/lock.c
+++ src/smp/lock.c
@@ -14,12 +14,12 @@ clh_lock_t big_kernel_lock ALIGN(L1_CACHE_LINE_SIZE);
 BOOT_CODE void clh_lock_init(void)
 {
     for (int i = 0; i < CONFIG_MAX_NUM_NODES; i++) {
-        big_kernel_lock.node_owners[i].node = &big_kernel_lock.nodes[i];
+        big_kernel_lock.node[i].myreq = &big_kernel_lock.request[i];
     }
 
-    /* Initialize the CLH head */
-    big_kernel_lock.nodes[CONFIG_MAX_NUM_NODES].value = CLHState_Granted;
-    big_kernel_lock.head = &big_kernel_lock.nodes[CONFIG_MAX_NUM_NODES];
+    /* Initialize the CLH tail */
+    big_kernel_lock.request[CONFIG_MAX_NUM_NODES].state = CLHState_Granted;
+    big_kernel_lock.tail = &big_kernel_lock.request[CONFIG_MAX_NUM_NODES];
 }
 
 #endif /* ENABLE_SMP_SUPPORT */
