09b635e80fb3a79bf2f69af1e90c15d76b674652
Indan Zupancic <Indan.Zupancic@mep-info.com>
2022-06-15 11:04:55 +0200

    SMP: Align lock data instead of explicit padding
    Signed-off-by: Indan Zupancic <Indan.Zupancic@mep-info.com>

--- src/smp/lock.c
+++ src/smp/lock.c
@@ -5,14 +5,18 @@
  */
 
 #include <config.h>
+#include <assert.h>
 #include <smp/lock.h>
 
 #ifdef ENABLE_SMP_SUPPORT
+compile_assert(BKL_not_padded, sizeof(big_kernel_lock) % L1_CACHE_LINE_SIZE == 0);
 
-clh_lock_t big_kernel_lock ALIGN(L1_CACHE_LINE_SIZE);
+clh_lock_t big_kernel_lock;
 
 BOOT_CODE void clh_lock_init(void)
 {
+    /* Check if linker honoured alignment */
+    assert(((seL4_Word)&big_kernel_lock) % L1_CACHE_LINE_SIZE == 0);
     for (int i = 0; i < CONFIG_MAX_NUM_NODES; i++) {
         big_kernel_lock.node[i].myreq = &big_kernel_lock.request[i];
     }
