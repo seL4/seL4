0c8c38639464f9cd8b88032302c9ddecf4ae38cb
Axel Heider <axel.heider@hensoldt.net>
2022-10-14 13:25:13 +0200

    fastpatch: use thread passed as parameter
    Use the thread passed as parameter instead of making assumption that
    this is in sync with the global state.
    
    Signed-off-by: Axel Heider <axel.heider@hensoldt.net>

--- include/arch/arm/arch/32/mode/fastpath/fastpath.h
+++ include/arch/arm/arch/32/mode/fastpath/fastpath.h
@@ -119,11 +119,11 @@ static inline void NORETURN FORCE_INLINE fastpath_restore(word_t badge, word_t m
     c_exit_hook();
 
 #ifdef CONFIG_ARM_CP14_SAVE_AND_RESTORE_NATIVE_THREADS
-    restore_user_debug_context(NODE_STATE(ksCurThread));
+    restore_user_debug_context(cur_thread);
 #endif
 
 #ifdef CONFIG_HAVE_FPU
-    lazyFPURestore(NODE_STATE(ksCurThread));
+    lazyFPURestore(cur_thread);
 #endif /* CONFIG_HAVE_FPU */
 
     register word_t badge_reg asm("r0") = badge;
