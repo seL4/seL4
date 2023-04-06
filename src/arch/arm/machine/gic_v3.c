a7845d297d7bcf32774d006929e809890a6bcf54
JorgeMVP <jorgepereira89@gmail.com>
2023-04-06 21:27:35 +0200

    gic_v3: fix offset for gicd iroutern mem-map reg
    GICD_IROUTERn is at the offset 0x6100 for SPI 32.
    SGIs and PPIs do not have a target since they are private to CPUs.
    Signed-off-by: JorgeMVP <jorgepereira89@gmail.com>

--- src/arch/arm/machine/gic_v3.c
+++ src/arch/arm/machine/gic_v3.c
@@ -173,7 +173,7 @@ BOOT_CODE static void dist_init(void)
     /* Route all global IRQs to this CPU */
     affinity = mpidr_to_gic_affinity();
     for (i = SPI_START; i < nr_lines; i++) {
-        gic_dist->iroutern[i] = affinity;
+        gic_dist->iroutern[i - SPI_START] = affinity;
     }
 }
 
@@ -381,7 +381,7 @@ void setIRQTarget(irq_t irq, seL4_Word target)
     }
 
     word_t hw_irq = IRQT_TO_IRQ(irq);
-    gic_dist->iroutern[hw_irq] = MPIDR_AFF_MASK(mpidr_map[target]);
+    gic_dist->iroutern[hw_irq - SPI_START] = MPIDR_AFF_MASK(mpidr_map[target]);
 }
 
 #endif /* ENABLE_SMP_SUPPORT */
