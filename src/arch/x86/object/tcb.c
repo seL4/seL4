3060d9b9634a45a71ed6ed7f739f0fec8bed896c
Axel Heider <axelheider@gmx.de>
2023-06-19 23:23:52 +0200

    remove obsolete assert()
    Commit f4c41f39 removed a check that dereferenced tcbSchedContext. It
    should have removed this assert() also then.
    
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- src/arch/x86/object/tcb.c
+++ src/arch/x86/object/tcb.c
@@ -84,10 +84,6 @@ exception_t decodeSetEPTRoot(cap_t cap)
 #ifdef ENABLE_SMP_SUPPORT
 void Arch_migrateTCB(tcb_t *thread)
 {
-#ifdef CONFIG_KERNEL_MCS
-    assert(thread->tcbSchedContext != NULL);
-#endif
-
     /* check if thread owns its current core FPU */
     if (nativeThreadUsingFPU(thread)) {
         switchFpuOwner(NULL, thread->tcbAffinity);
