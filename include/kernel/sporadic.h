97588df01677e3a13f2d1a0d40cc1ba218cb444a
Gerwin Klein <gerwin.klein@proofcraft.systems>
2023-11-06 13:56:27 +1100

    mcs: simplify sc_sporadic, assert invariant
    It is an invariant in the MCS kernel that scSporadic implies sc_active.
    Make use of that invariant by avoiding an explicit check for sc_active,
    but assert the invariant in debug mode so it fails quickly when new
    code breaks the invariant.
    
    Essentially reverts 56098195f21f now that the invariant is preserved,
    but adjusts the assertion from 17109eb8c986cd0ac to actually express
    the invariant.
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- include/kernel/sporadic.h
+++ include/kernel/sporadic.h
@@ -160,7 +160,10 @@ static inline bool_t sc_released(sched_context_t *sc)
  */
 static inline bool_t sc_sporadic(sched_context_t *sc)
 {
-    return sc != NULL && sc_active(sc) && sc->scSporadic;
+    /* asserting sc != NULL --> sc->scSporadic --> sc_active(sc). That means, when
+       this function returns true, we also know that sc_active(sc) is true */
+    assert(sc == NULL || !sc->scSporadic || sc_active(sc));
+    return sc != NULL && sc->scSporadic;
 }
 
 /*
