868febc13f1fb1f387367e62d94a91957380c20c
Hesham Almatary <hesham.almatary@cl.cam.ac.uk>
2023-11-29 12:48:53 +0000

    morello: Add a new Morello CPU
    This is an initial commit to support building and running
    seL4 on the new Arm's Morello CPU [1] which has CHERI extension [2].
    However, this commit only supports AArch64 without CHERI yet. This
    servers as a baseline architecture for future CHERI support.
    
    [1] https://www.morello-project.org
    [2] https://www.cl.cam.ac.uk/research/security/ctsrd/cheri/
    
    Signed-off-by: Hesham Almatary <hesham.almatary@cl.cam.ac.uk>

---
+++ libsel4/sel4_plat_include/morello-qemu/sel4/plat/api/constants.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <sel4/config.h>
+
+#include <sel4/arch/constants_morello.h>
