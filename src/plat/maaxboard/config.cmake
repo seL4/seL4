e59e1fdabce1925dbd7583c8c7052a47cc446fac
Josh Felmeden <josh.felmeden@capgemini.com>
2024-01-17 17:23:31 +0000

    added required files for maaxboard support microkit
    Signed-off-by: Josh Felmeden <josh.felmeden@capgemini.com>

--- src/plat/maaxboard/config.cmake
+++ src/plat/maaxboard/config.cmake
@@ -7,23 +7,21 @@
 
 cmake_minimum_required(VERSION 3.7.2)
 
-declare_platform(imx8mq-evk KernelPlatformImx8mq-evk PLAT_IMX8MQ_EVK KernelArchARM)
-declare_platform(imx8mm-evk KernelPlatformImx8mm-evk PLAT_IMX8MM_EVK KernelArchARM)
+declare_platform(maaxboard KernelPlatformMaaxboard PLAT_MAAXBOARD KernelArchARM)
 
-if(KernelPlatformImx8mq-evk OR KernelPlatformImx8mm-evk)
+if(KernelPlatformMaaxboard)
     declare_seL4_arch(aarch64 aarch32)
-    if(KernelPlatformImx8mq-evk)
-        config_set(KernelPlatImx8mq PLAT_IMX8MQ ON)
-    endif()
+    config_set(KernelPlatImx8mq PLAT_IMX8MQ ON)
+
     set(KernelArmCortexA53 ON)
     set(KernelArchArmV8a ON)
     set(KernelArmGicV3 ON)
     config_set(KernelARMPlatform ARM_PLAT ${KernelPlatform})
     set(KernelArmMach "imx" CACHE INTERNAL "")
     list(APPEND KernelDTSList "tools/dts/${KernelPlatform}.dts")
-    list(APPEND KernelDTSList "src/plat/imx8m-evk/overlay-${KernelPlatform}.dts")
+    list(APPEND KernelDTSList "src/plat/maaxboard/overlay-${KernelPlatform}.dts")
     if(KernelSel4ArchAarch32)
-        list(APPEND KernelDTSList "src/plat/imx8m-evk/overlay-imx8m-32bit.dts")
+        list(APPEND KernelDTSList "src/plat/maaxboard/overlay-${KernelPlatform}-32bit.dts")
     endif()
     declare_default_headers(
         TIMER_FREQUENCY 8000000
@@ -38,6 +36,6 @@ if(KernelPlatformImx8mq-evk OR KernelPlatformImx8mm-evk)
 endif()
 
 add_sources(
-    DEP "KernelPlatformImx8mq-evk OR KernelPlatformImx8mm-evk"
+    DEP "KernelPlatformMaaxboard"
     CFILES src/arch/arm/machine/gic_v3.c src/arch/arm/machine/l2c_nop.c
 )
