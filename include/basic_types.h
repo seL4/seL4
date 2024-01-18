7497114162dbad0c86b3e8a0e17fd815d7750f7a
Axel Heider <axelheider@gmx.de>
2024-01-18 18:12:22 +0100

    simplify include files for types
    - Keep basic_types.h simple and move struct definitions to types.h
    - The header file types.h is the only file that includes
      compound_types.h, so the content can be merged there
    
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- include/basic_types.h
+++ include/basic_types.h
@@ -33,33 +33,6 @@ enum _bool {
 };
 typedef word_t bool_t;
 
-/**
- * A region [start..end) of kernel-virtual memory.
- *
- * Empty when start == end. If end < start, the region wraps around, that is,
- * it represents the addresses in the set [start..-1] union [0..end). This is
- * possible after address translation and fine for e.g. device memory regions.
- */
-typedef struct region {
-    pptr_t start; /* inclusive */
-    pptr_t end;   /* exclusive */
-} region_t;
-
-/** A region [start..end) of physical memory addresses. */
-typedef struct p_region {
-    paddr_t start; /* inclusive */
-    paddr_t end;   /* exclusive */
-} p_region_t;
-
-/** A region [start..end) of user-virtual addresses. */
-typedef struct v_region {
-    vptr_t start; /* inclusive */
-    vptr_t end;   /* exclusive */
-} v_region_t;
-
-#define REG_EMPTY (region_t){ .start = 0, .end = 0 }
-#define P_REG_EMPTY (p_region_t){ .start = 0, .end = 0 }
-
 /* equivalent to a word_t except that we tell the compiler that we may alias with
  * any other type (similar to a char pointer) */
 typedef word_t __attribute__((__may_alias__)) word_t_may_alias;
