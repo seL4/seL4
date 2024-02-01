5c07956943dfe6d2b35f231524ac4c1f4395612c
Kent McLeod <kent@kry10.com>
2024-02-02 08:05:38 +1100

    gic_v3,smp: Deactivate PPIs on different cores
    This is added for completeness, but it's not efficient for userlevel to
    deactivate PPI IRQs on different cores than they were received on.
    
    Signed-off-by: Kent McLeod <kent@kry10.com>

--- src/arch/arm/smp/ipi.c
+++ src/arch/arm/smp/ipi.c
@@ -59,6 +59,12 @@ static void handleRemoteCall(IpiModeRemoteCall_t call, word_t arg0,
             maskInterrupt(arg0, IDX_TO_IRQT(arg1));
             break;
 
+#ifdef CONFIG_ARM_GIC_V3_SUPPORT
+        case IpiRemoteCall_DeactivatePrivateInterrupt:
+            deactivateInterrupt(IDX_TO_IRQT(arg1));
+            break;
+#endif
+
 #if defined CONFIG_ARM_HYPERVISOR_SUPPORT && defined ENABLE_SMP_SUPPORT
         case IpiRemoteCall_VCPUInjectInterrupt: {
             virq_t virq;
