271f6cc90304edeaa731dab95bd81a90de801046
Axel Heider <axelheider@gmx.de>
2024-03-11 14:37:36 +0100

    risc-v: make trap_entry a function
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- include/arch/riscv/arch/kernel/traps.h
+++ include/arch/riscv/arch/kernel/traps.h
@@ -20,6 +20,9 @@ static inline void arch_c_exit_hook(void)
     /* Nothing architecture specific to be done. */
 }
 
+/* ASM trap entry. */
+void NORETURN trap_entry(void);
+
 #ifdef CONFIG_KERNEL_MCS
 void c_handle_fastpath_reply_recv(word_t cptr, word_t msgInfo, word_t reply)
 #else
