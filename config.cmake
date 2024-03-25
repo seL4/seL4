adc33649b1d9abfc352c5bddbd381ac6687c1750
Axel Heider <axel.heider@codasip.com>
2024-03-25 23:24:44 +0100

    trivial/cmake: fix typo in description
    Signed-off-by: Axel Heider <axel.heider@codasip.com>

--- config.cmake
+++ config.cmake
@@ -356,7 +356,7 @@ config_string(
     KernelFPUMaxRestoresSinceSwitch FPU_MAX_RESTORES_SINCE_SWITCH
     "This option is a heuristic to attempt to detect when the FPU is no longer in use,\
     allowing the kernel to save the FPU state out so that the FPU does not have to be\
-    enabled/disabled every thread swith. Every time we restore a thread and there is\
+    enabled/disabled every thread switch. Every time we restore a thread and there is\
     active FPU state, we increment this setting and if it exceeds this threshold we\
     switch to the NULL state."
     DEFAULT 64
