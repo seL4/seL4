8cbc13d5b8178f46b66c3843da8ae140c8a67675
Indan Zupancic <indan@nul.nu>
2024-03-26 13:26:41 +0000

    Manual: Document ARM Virtualisation
    This resolves issue #1083.
    
    Signed-off-by: Indan Zupancic <indan@nul.nu>

--- libsel4/sel4_arch_include/aarch64/sel4/sel4_arch/constants.h
+++ libsel4/sel4_arch_include/aarch64/sel4/sel4_arch/constants.h
@@ -52,8 +52,6 @@ typedef enum {
     SEL4_FORCE_LONG_ENUM(seL4_VMFault_Msg),
 } seL4_VMFault_Msg;
 
-#ifdef CONFIG_ARM_HYPERVISOR_SUPPORT
-
 typedef enum {
     seL4_VGICMaintenance_IDX,
     seL4_VGICMaintenance_Length,
@@ -114,8 +112,6 @@ typedef enum {
     seL4_VCPUReg_Num,
     SEL4_FORCE_LONG_ENUM(seL4_VCPUReg),
 } seL4_VCPUReg;
-
-#endif /* CONFIG_ARM_HYPERVISOR_SUPPORT */
 
 #ifdef CONFIG_KERNEL_MCS
 typedef enum {
