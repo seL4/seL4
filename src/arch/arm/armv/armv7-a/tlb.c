f03bcab247abb8bb0ddfa75f3727a78a0ffce251
Gerwin Klein <gerwin.klein@proofcraft.systems>
2024-05-02 14:58:49 +1000

    arm tlb: only perform TLB lockdown for Cortex A8
    The code previously used the same instructions for Cortex A8 and A9,
    but the Cortex A8 instructions are undocumented for A9, and A9 provides
    a slightly different TLB interface.
    
    As far as we can tell, the instructions were simply ignored by the
    supported A8 platforms, so there was no current correctness issue.
    
    Since the instructions had no effect, this commit removes A9 TLB
    lockdown support.
    
    This potential issue was discovered and reported by the UK's National
    Cyber Security Centre (NCSC).
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- src/arch/arm/armv/armv7-a/tlb.c
+++ src/arch/arm/armv/armv7-a/tlb.c
@@ -6,14 +6,7 @@
 
 #include <arch/machine/hardware.h>
 
-#if defined(CONFIG_ARM_CORTEX_A15) || defined(CONFIG_ARM_CORTEX_A7)
-/* The hardware does not support tlb locking */
-void lockTLBEntry(vptr_t vaddr)
-{
-
-}
-
-#else
+#if defined(CONFIG_ARM_CORTEX_A8)
 
 void lockTLBEntry(vptr_t vaddr)
 {
@@ -23,32 +16,22 @@ void lockTLBEntry(vptr_t vaddr)
     tlbLockCount ++;
     /* Compute two values, x and y, to write to the lockdown register. */
 
-#if defined(CONFIG_ARM_CORTEX_A8)
-
     /* Before lockdown, base = victim = num_locked_tlb_entries. */
     x = 1 | (n << 22) | (n << 27);
     n ++;
     /* After lockdown, base = victim = num_locked_tlb_entries + 1. */
     y = (n << 22) | (n << 27);
 
-#elif defined(CONFIG_ARM_CORTEX_A9)
-
-    /* Before lockdown, victim = num_locked_tlb_entries. */
-    x = 1 | (n << 28);
-    n ++;
-    /* After lockdown, victim = num_locked_tlb_entries + 1. */
-    y = (n << 28);
-
-#else
-
-    userError("Undefined CPU for TLB lockdown.\n");
-    halt();
-
-#endif /* A8/A9 */
-
     lockTLBEntryCritical(vaddr, x, y);
 }
 
-#endif /* A15/A7 */
+/* if CORTEX_A8 */
+#else
 
+/* We don't currently support TLB locking for other processors. */
+void lockTLBEntry(vptr_t vaddr)
+{
 
+}
+
+#endif
