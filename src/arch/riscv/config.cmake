1cca12ba2e2b0249461a87b289a6c6d2b3e2f5a5
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-06-11 16:08:39 +0800

    Disable KernelRiscvUseClintMtime by default
    This configuration option requires access to the CLINT in S-mode
    which is not available on newer versions of OpenSBI. Because of
    this, it makes more sense to not turn on this option by default
    and instead let it be 'opt-in' for those who want the performance
    benefit and also have firmware that does not restrict S-mode access
    to the CLINT.
    
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- src/arch/riscv/config.cmake
+++ src/arch/riscv/config.cmake
@@ -32,7 +32,8 @@ config_option(
     from the hardware, directly access the CLINT timer register (mtime) instead \
     of using the rdtime instruction. This is a performance optimization, but \
     only for platforms where executing the rdtime instruction results in a \
-    trap into M-mode software which then accesses the CLINT timer register."
+    trap into M-mode software which then accesses the CLINT timer register. \
+    Note that this option requires S-mode access to the CLINT."
     DEFAULT OFF
     DEPENDS "KernelArchRiscV"
 )
