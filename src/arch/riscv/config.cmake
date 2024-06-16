86ade996b2757316cc7f2325d407b0648802d567
Gerwin Klein <gerwin.klein@proofcraft.systems>
2024-06-16 14:25:54 +1000

    cmake: style fix
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- src/arch/riscv/config.cmake
+++ src/arch/riscv/config.cmake
@@ -10,8 +10,8 @@ config_string(
     KernelPTLevels PT_LEVELS "Number of page \
     table levels for RISC-V depends on the mode. For example there are: \
     2, 3 and 4 levels on Sv32, Sv39, Sv48 RISC-V paging modes respectively."
-    DEFAULT 3 UNDEF_DISABLED
-    UNQUOTE
+    DEFAULT 3
+    UNDEF_DISABLED UNQUOTE
     DEPENDS "KernelArchRiscV"
 )
 
