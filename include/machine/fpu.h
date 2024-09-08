ed5b1d3fc73a12f8731605e075d81614926cbd0a
Indan Zupancic <indan@nul.nu>
2024-09-08 21:28:47 +0100

    FPU: Save and restore FPU state based on TCB flag
    Remove fault-based FPU state saving and loading.
    
    Signed-off-by: Indan Zupancic <indan@nul.nu>

--- include/machine/fpu.h
+++ include/machine/fpu.h
@@ -16,9 +16,6 @@
 /* Perform any actions required for the deletion of the given thread. */
 void fpuThreadDelete(tcb_t *thread);
 
-/* Handle an FPU exception. */
-exception_t handleFPUFault(void);
-
 void switchLocalFpuOwner(user_fpu_state_t *new_owner);
 
 /* Switch the current owner of the FPU state on the core specified by 'cpu'. */
@@ -31,28 +28,15 @@ static inline bool_t nativeThreadUsingFPU(tcb_t *thread)
            NODE_STATE_ON_CORE(ksActiveFPUState, thread->tcbAffinity);
 }
 
+/* Called without global lock held! */
 static inline void FORCE_INLINE lazyFPURestore(tcb_t *thread)
 {
-    if (unlikely(NODE_STATE(ksActiveFPUState))) {
-        /* If we have enabled/disabled the FPU too many times without
-         * someone else trying to use it, we assume it is no longer
-         * in use and switch out its state. */
-        if (unlikely(NODE_STATE(ksFPURestoresSinceSwitch) > CONFIG_FPU_MAX_RESTORES_SINCE_SWITCH)) {
-            switchLocalFpuOwner(NULL);
-            NODE_STATE(ksFPURestoresSinceSwitch) = 0;
-        } else {
-            if (likely(nativeThreadUsingFPU(thread))) {
-                /* We are using the FPU, make sure it is enabled */
-                enableFpu();
-            } else {
-                /* Someone is using the FPU and it might be enabled */
-                disableFpu();
-            }
-            NODE_STATE(ksFPURestoresSinceSwitch)++;
-        }
+    if (thread->flags & seL4_TCBFlag_fpuDisabled) {
+        disableFpu();
+    } else if (nativeThreadUsingFPU(thread)) {
+        enableFpu();
     } else {
-        /* No-one (including us) is using the FPU, so we assume it
-         * is currently disabled */
+        switchLocalFpuOwner(&thread->tcbArch.tcbContext.fpuState);
     }
 }
 
