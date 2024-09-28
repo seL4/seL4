578df2117ac9e98e4c9d8c65e55a16b4205075ab
Gerwin Klein <gerwin.klein@proofcraft.systems>
2024-09-28 09:20:54 +0200

    configs: additional verified platforms
    With recent proof improvements the proofs now apply to further platforms
    in the ARM and AARCH64 configurations.
    
    Refactor the verified configs to build on one include file per major
    architecture which is then used for each platform with potentially
    modified settings. Add path argument to `cmake_script_build_kernel`
    macro to accommodate inclusion from different locations in the file
    system.
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

---
+++ configs/include/ARM_HYP_verified_include.cmake
@@ -0,0 +1,9 @@
+#
+# Copyright 2024, Proofcraft Pty Ltd
+#
+# SPDX-License-Identifier: GPL-2.0-only
+#
+
+include(${CMAKE_CURRENT_LIST_DIR}/ARM_verified_include.cmake)
+
+set(KernelSel4Arch "arm_hyp" CACHE STRING "" FORCE)
