ec31db36167354ab176fa353f9c70af19960d728
Hesham Almatary <heshamalmatary@capabilitieslimited.co.uk>
2024-10-29 14:01:21 +0000

    [cheri][riscv] low-level architecture port
    Signed-off-by: Hesham Almatary <heshamalmatary@capabilitieslimited.co.uk>

--- include/arch/riscv/arch/machine/hardware.h
+++ include/arch/riscv/arch/machine/hardware.h
@@ -66,8 +66,9 @@ enum vm_fault_type {
     RISCVInstructionPageFault = 12,
     RISCVLoadPageFault = 13,
     /* 14 - reserved */
-    RISCVStorePageFault = 15
-                          /* >= 16 reserved */
+    RISCVStorePageFault = 15,
+    RISCVCheriFault = 0x1c
+                      /* >= 16 reserved */
 };
 typedef word_t vm_fault_type_t;
 
