75e1c609744e68e3308c08f6297536bc9a4358c0
Indan Zupancic <indan@nul.nu>
2025-02-18 23:22:16 +0000

    Arm, L2C-310: Avoid memory reordering
    The CoreLink L2C-310 cache controller is used by:
    
    - imx6
    - exynos4
    - zynq7000
    
    Make sure previous memory accesses finish before starting cache
    management operations like cleaning and flushing the L2 cache.
    
    Prevent later memory accesses being reordered to before the flush.
    
    Signed-off-by: Indan Zupancic <indan@nul.nu>

--- src/arch/arm/machine/l2c_310.c
+++ src/arch/arm/machine/l2c_310.c
@@ -340,6 +340,9 @@ BOOT_CODE void initL2Cache(void)
     /* Direct register access */
     l2cc->control.control |= CTRL_CTRL_EN;
 #endif /* TI_MSHIELD */
+    /* Guarantee that the setACTLR() doesn't start executing until the completion
+     * of the l2cc register updates */
+    dsb();
 
 #if defined(CONFIG_ARM_CORTEX_A9) && defined(CONFIG_ENABLE_A9_PREFETCHER)
     /* Set bit 1 in the ACTLR, which on the cortex-a9 is the l2 prefetch enable
@@ -352,14 +355,20 @@ BOOT_CODE void initL2Cache(void)
 
 static inline void L2_cacheSync(void)
 {
+    /* Avoid normal memory writes being reordered with device memory writes */
     dmb();
     l2cc->maintenance.cache_sync = 0;
     while (l2cc->maintenance.cache_sync & MAINTENANCE_PENDING);
+    /* Wait till the L2 cache operation has completed and avoid later memoy
+     * operations being reordered to before this point */
+    dsb();
 }
 
 void plat_cleanInvalidateL2Cache(void)
 {
     if (!config_set(CONFIG_DEBUG_DISABLE_L2_CACHE)) {
+        /* Avoid normal memory writes being reordered with device memory writes */
+        dmb();
         l2cc->maintenance.clean_way = 0xffff;
         while (l2cc->maintenance.clean_way);
         L2_cacheSync();
@@ -372,6 +381,8 @@ void plat_cleanInvalidateL2Cache(void)
 void plat_cleanCache(void)
 {
 #ifndef CONFIG_DEBUG_DISABLE_L2_CACHE
+    /* Avoid normal memory writes being reordered with device memory writes */
+    dmb();
     /* Clean by way. */
     l2cc->maintenance.clean_way = 0xffff;
     while (l2cc->maintenance.clean_way & 0xffff);
@@ -385,6 +396,8 @@ void plat_cleanL2Range(paddr_t start, paddr_t end)
     /* Documentation specifies this as the only possible line size */
     assert(((l2cc->id.cache_type >> 12) & 0x3) == 0x0);
 
+    /* Avoid normal memory writes being reordered with device memory writes */
+    dmb();
     for (start = L2_LINE_START(start);
          start != L2_LINE_START(end + L2_LINE_SIZE);
          start += L2_LINE_SIZE) {
@@ -401,9 +414,10 @@ void plat_invalidateL2Range(paddr_t start, paddr_t end)
     /* Documentation specifies this as the only possible line size */
     assert(((l2cc->id.cache_type >> 12) & 0x3) == 0x0);
 
+    /* Avoid normal memory writes being reordered with device memory writes */
+    dmb();
     /* We assume that if this is a partial line that whoever is calling us
      * has already done the clean, so we just blindly invalidate all the lines */
-
     for (start = L2_LINE_START(start);
          start != L2_LINE_START(end + L2_LINE_SIZE);
          start += L2_LINE_SIZE) {
@@ -420,6 +434,8 @@ void plat_cleanInvalidateL2Range(paddr_t start, paddr_t end)
     /* Documentation specifies this as the only possible line size */
     assert(((l2cc->id.cache_type >> 12) & 0x3) == 0x0);
 
+    /* Avoid normal memory writes being reordered with device memory writes */
+    dmb();
     for (start = L2_LINE_START(start);
          start != L2_LINE_START(end + L2_LINE_SIZE);
          start += L2_LINE_SIZE) {
