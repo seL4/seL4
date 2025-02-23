1f2cd16b16df462fe64eca060248ec507c2b8ed8
Indan Zupancic <indan@nul.nu>
2025-02-23 21:24:53 +0000

    Aarch32, SMP: Ensure SP points within region
    The logical core ID is encoded in the stack pointer.
    
    But because the stack is fully decending on Arm, the initial SP
    points to the (exclusive) top address of the region allocated
    as stack for this core. To prevent this boundary value causing
    problems in the current core calculation, substract 8 from the
    initial SP value.
    
    Signed-off-by: Indan Zupancic <indan@nul.nu>

--- src/arch/arm/kernel/boot.c
+++ src/arch/arm/kernel/boot.c
@@ -206,9 +206,14 @@ BOOT_CODE static bool_t init_cpu(void)
      * On ARM SMP, the array index here is the CPU ID
      */
     word_t stack_top = ((word_t) kernel_stack_alloc[CURRENT_CPU_INDEX()]) + BIT(CONFIG_KERNEL_STACK_BITS);
-#if defined(ENABLE_SMP_SUPPORT) && defined(CONFIG_ARCH_AARCH64)
+#ifdef ENABLE_SMP_SUPPORT
+#ifdef CONFIG_ARCH_AARCH64
     /* the least 12 bits are used to store logical core ID */
     stack_top |= getCurrentCPUIndex();
+#elif defined(CONFIG_ARCH_AARCH32)
+    /* Stack address encodes core ID, ensure it is in the right region */
+    stack_top -= 8;
+#endif
 #endif
     setKernelStack(stack_top);
 
