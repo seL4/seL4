0ee99af5ff1371dec5e61c522d0faff3b74b381c
Liu, Chang <cl91tp@gmail.com>
2025-10-20 11:04:51 +0800

    x86: pass EFI system table pointer to root task
    Pass the EFI system table pointer supplied by the bootloader in
    the multiboot2 information structure to the root task, in the form
    of an extra bootinfo structure. The EFI system table contains
    pointers to firmware tables such as SMBIOS tables, which userspace
    tasks can then map using standard seL4 APIs.
    
    Signed-off-by: Liu, Chang <cl91tp@gmail.com>

--- libsel4/include/sel4/bootinfo_types.h
+++ libsel4/include/sel4/bootinfo_types.h
@@ -106,6 +106,7 @@ typedef enum {
     SEL4_BOOTINFO_HEADER_X86_FRAMEBUFFER    = 4,
     SEL4_BOOTINFO_HEADER_X86_TSC_FREQ       = 5, /* frequency is in MHz */
     SEL4_BOOTINFO_HEADER_FDT                = 6, /* device tree */
+    SEL4_BOOTINFO_HEADER_EFI_SYSTBL_PTR     = 7, /* EFI system table pointer */
     /* Add more IDs here, the two elements below must always be at the end. */
     SEL4_BOOTINFO_HEADER_NUM,
     SEL4_FORCE_LONG_ENUM(seL4_BootInfoID)
