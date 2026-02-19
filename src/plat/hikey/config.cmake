29df9eca06737583b71d9c520d2b7f1959f1b7d5
Krishnan Winter <krishnan.winter@unsw.edu.au>
2026-02-19 11:57:29 +1100

    config: fix hikey config evaluation
    In the hikey case, it relies on definitions from
    the root config.cmake file in seL4, which is
    called after the platform specific configs.
    
    In the case of KernelDebugDisablePrefetchers,
    this is only set to off by default for x86
    and hikey configs. As all platform configs are
    included by seL4Config we also evaluate all of
    their configs. We don't want to set
    KernelDebugPrefetchers to OFF when we are building
    other platforms, so we move it inside the if
    platform == hikey check.
    
    Signed-off-by: Krishnan Winter <krishnan.winter@unsw.edu.au>

--- src/plat/hikey/config.cmake
+++ src/plat/hikey/config.cmake
@@ -24,59 +24,67 @@ if(KernelPlatformHikey)
         CLK_SHIFT 39u
         KERNEL_WCET 10u
     )
+    if(NOT DEFINED KernelDebugDisablePrefetchers)
+        # This is a specific case for the hikey. KernelDebugDisablePrefetchers
+        # is set in seL4/config.cmake, and will be set after the platform specific
+        # configs are evaluated. This is explicitly setting this to off, as if
+        # the user hasn't passed this config when building, it would have defaulted
+        # to off for this platform anyways.
+
+        set(KernelDebugDisablePrefetchers OFF INTERNAL)
+    endif()
+    config_string(
+        KernelArmHikeyOutstandingPrefetchers ARM_HIKEY_OUTSTANDING_PREFETCHERS
+        "Number of outstanding prefetch allowed \
+        Cortex A53 has an L1 Data prefetcher. This config options allows \
+        the number of outstanding prefetcher to be set from a number from \
+        1 to 7. Note that a setting of 7 maps to 8 and 5 is the reset value."
+        DEFAULT 5
+        DEPENDS "KernelPlatformHikey;NOT KernelDebugDisablePrefetchers" DEFAULT_DISABLED 0
+        UNQUOTE
+    )
+    set_property(CACHE KernelArmHikeyOutstandingPrefetchers PROPERTY STRINGS "1;2;3;4;5;6;7")
+
+    config_string(
+        KernelArmHikeyPrefetcherStride ARM_HIKEY_PREFETCHER_STRIDE
+        "Number of strides before prefetcher is triggered \
+        Number of strides before prefetcher is triggered. \
+        Allowed values are 2 and 3. 2 is the reset value"
+        DEFAULT 2
+        DEPENDS "KernelPlatformHikey;NOT KernelDebugDisablePrefetchers" DEFAULT_DISABLED 0
+        UNQUOTE
+    )
+    set_property(CACHE KernelArmHikeyPrefetcherStride PROPERTY STRINGS "2;3")
+
+    config_string(
+        KernelArmHikeyPrefetcherNPFSTRM ARM_HIKEY_PREFETCHER_NPFSTRM
+        "Number of independent prefetch streams \
+        Number of independent prefetch streams. Allowed values are 1 to 4.\
+        2 is the reset value"
+        DEFAULT 2
+        DEPENDS "KernelPlatformHikey;NOT KernelDebugDisablePrefetchers" DEFAULT_DISABLED 0
+        UNQUOTE
+    )
+    set_property(CACHE KernelArmHikeyPrefetcherNPFSTRM PROPERTY STRINGS "1;2;3;4")
+
+    config_option(
+        KernelArmHikeyPrefetcherSTBPFDIS ARM_HIKEY_PREFETCHER_STBPFDIS
+        "Enable prefetch streams initated by STB access \
+        Enable prefetch streams initated by STB access. Enabled is the reset value"
+        DEFAULT ON
+        DEPENDS "KernelPlatformHikey; NOT KernelDebugDisablePrefetchers"
+        DEFAULT_DISABLED OFF
+    )
+
+    config_option(
+        KernelArmHikeyPrefetcherSTBPFRS ARM_HIKEY_PREFETCHER_STBPFRS
+        "Prefetcher to initated on a ReadUnique or ReadShared \
+        Sets prefetcher to initated on a ReadUnique (n) or ReadShared (y) \
+        ReadUnique is the reset value"
+        DEFAULT OFF
+        DEPENDS "KernelPlatformHikey;NOT KernelDebugDisablePrefetchers"
+    )
 endif()
-
-config_string(
-    KernelArmHikeyOutstandingPrefetchers ARM_HIKEY_OUTSTANDING_PREFETCHERS
-    "Number of outstanding prefetch allowed \
-    Cortex A53 has an L1 Data prefetcher. This config options allows \
-    the number of outstanding prefetcher to be set from a number from \
-    1 to 7. Note that a setting of 7 maps to 8 and 5 is the reset value."
-    DEFAULT 5
-    DEPENDS "KernelPlatformHikey;NOT KernelDebugDisablePrefetchers" DEFAULT_DISABLED 0
-    UNQUOTE
-)
-set_property(CACHE KernelArmHikeyOutstandingPrefetchers PROPERTY STRINGS "1;2;3;4;5;6;7")
-
-config_string(
-    KernelArmHikeyPrefetcherStride ARM_HIKEY_PREFETCHER_STRIDE
-    "Number of strides before prefetcher is triggered \
-    Number of strides before prefetcher is triggered. \
-    Allowed values are 2 and 3. 2 is the reset value"
-    DEFAULT 2
-    DEPENDS "KernelPlatformHikey;NOT KernelDebugDisablePrefetchers" DEFAULT_DISABLED 0
-    UNQUOTE
-)
-set_property(CACHE KernelArmHikeyPrefetcherStride PROPERTY STRINGS "2;3")
-
-config_string(
-    KernelArmHikeyPrefetcherNPFSTRM ARM_HIKEY_PREFETCHER_NPFSTRM
-    "Number of independent prefetch streams \
-    Number of independent prefetch streams. Allowed values are 1 to 4.\
-    2 is the reset value"
-    DEFAULT 2
-    DEPENDS "KernelPlatformHikey;NOT KernelDebugDisablePrefetchers" DEFAULT_DISABLED 0
-    UNQUOTE
-)
-set_property(CACHE KernelArmHikeyPrefetcherNPFSTRM PROPERTY STRINGS "1;2;3;4")
-
-config_option(
-    KernelArmHikeyPrefetcherSTBPFDIS ARM_HIKEY_PREFETCHER_STBPFDIS
-    "Enable prefetch streams initated by STB access \
-    Enable prefetch streams initated by STB access. Enabled is the reset value"
-    DEFAULT ON
-    DEPENDS "KernelPlatformHikey; NOT KernelDebugDisablePrefetchers"
-    DEFAULT_DISABLED OFF
-)
-
-config_option(
-    KernelArmHikeyPrefetcherSTBPFRS ARM_HIKEY_PREFETCHER_STBPFRS
-    "Prefetcher to initated on a ReadUnique or ReadShared \
-    Sets prefetcher to initated on a ReadUnique (n) or ReadShared (y) \
-    ReadUnique is the reset value"
-    DEFAULT OFF
-    DEPENDS "KernelPlatformHikey;NOT KernelDebugDisablePrefetchers"
-)
 
 add_sources(
     DEP "KernelPlatformHikey"
