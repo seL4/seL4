4c6209826532d7ff26f6a6f02ec660a7cdef9980
Gerwin Klein <gerwin.klein@proofcraft.systems>
2026-08-25 12:40:02 +0200

    aarch64,types: make hw_asid_t 16 bit for non-hyp
    Previously the AArch64 non-hyp code expected 16 bit hardware ASIDs, but
    used an 8-bit type, with the consequence of flushing the wrong TLB
    entries when there are more than 256 threads.
    
    For VMIDs, seL4 currently only supports 8 bit, which works correctly
    for 8 and 16 bit support from the hardware side.
    
    Fixes #1735
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- include/arch/arm/arch/64/mode/types.h
+++ include/arch/arm/arch/64/mode/types.h
@@ -14,3 +14,9 @@ compile_assert(long_is_64bits, sizeof(unsigned long) == 8)
 #define wordBits (1 << wordRadix)
 
 typedef uint64_t timestamp_t;
+
+#ifdef CONFIG_ARM_HYPERVISOR_SUPPORT
+typedef uint8_t  hw_asid_t;
+#else
+typedef uint16_t hw_asid_t;
+#endif
--- include/arch/arm/arch/types.h
+++ include/arch/arm/arch/types.h
@@ -23,8 +23,6 @@ typedef word_t node_id_t;
 typedef word_t cpu_id_t;
 typedef word_t dom_t;
 
-typedef uint8_t  hw_asid_t;
-
 enum hwASIDConstants {
     hwASIDMax = 255,
     hwASIDBits = 8
